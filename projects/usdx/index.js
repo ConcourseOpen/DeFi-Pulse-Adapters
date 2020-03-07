e358b69c6db0b23e06a9a0dea2bc4da0c9755f1c
skyge <18844062006@163.com>
2020-03-07 17:04:21 +0800

    U Update USDx adapter.

--- projects/usdx/index.js
+++ projects/usdx/index.js
@@ -1,7 +1,6 @@
 /*==================================================
   Modules
   ==================================================*/
-
   const sdk = require('../../sdk');
   const _ = require('underscore');
   const BigNumber = require('bignumber.js');
@@ -10,40 +9,22 @@
   Settings
   ==================================================*/
 
-  const reserves = [
-    '0x8E870D67F660D95d5be530380D0eC0bd388289E1', // PAX
-    '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
-    '0x0000000000085d4780B73119b644AE5ecd22b376'  // TUSD
-  ]
-
-  const poolCore = "0x786bF554473f9aB733Fd683C528212492A23D895" // dForce: Stablecoin Pool
+  const asset = "0xeb269732ab75A6fD61Ea60b06fE994cD32a83549" // USDx
 
 /*==================================================
   Main
   ==================================================*/
 
   async function run(timestamp, block) {
-    let balances = {};
+    let balance = {};
 
-    const calls = _.reduce(reserves, (accum, reserve) => [...accum, {
-        target: reserve,
-        params: poolCore
-    }] , []);
-
-    let balanceOfResults = await sdk.api.abi.multiCall({
-      block,
-      calls,
-      abi: 'erc20:balanceOf'
+    let totalSupplyResult = await sdk.api.erc20.totalSupply({
+      target: asset,
     });
 
-    _.each(balanceOfResults.output, (balanceOf) => {
-      if(balanceOf.success) {
-        let address = balanceOf.input.target
-        balances[address] = BigNumber(balances[address] || 0).plus(balanceOf.output).toFixed();
-      }
-    });
+    balance[asset] = BigNumber(totalSupplyResult.output);
 
-    return (await sdk.api.util.toSymbols(balances)).output;
+    return (await sdk.api.util.toSymbols(balance)).output;
   }
 
 /*==================================================
@@ -57,4 +38,3 @@
     start: 1563991581, // Jul-25-2019 02:06:21 AM +UTC
     run
   }
-
