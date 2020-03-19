168a1ace0291667aaa8aea060a0d12ebead024a1
Matthew Pilkey <m.pilkey@gmail.com>
2020-03-19 13:26:08 -0500

    reconcile with PR

--- projects/nexus/index.js
+++ projects/nexus/index.js
@@ -10,28 +10,32 @@
   Settings
   ==================================================*/
 
-  const tokenAddresses = [
-    '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359', // SAI
+  const pools = [
+    '0xfd61352232157815cf7b71045557192bf0ce1884',  // currency assets
+    '0x7cbe5682be6b648cc1100c76d4f6c96997f753d6'   // investment assets
   ];
 
-  const poolAddress = '0xfd61352232157815cf7b71045557192bf0ce1884'
+  const tokensAddresses = [
+    '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+    '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359', // SAI
+  ]
 
 /*==================================================
   Main
   ==================================================*/
 
   async function run(timestamp, block) {
-    let getBalance = await sdk.api.eth.getBalance({target: poolAddress, block});
+    let balances = {};
 
-    let balances = {
-      '0x0000000000000000000000000000000000000000': getBalance.output
-    };
+    let calls = [];
 
-    let calls = _.map(tokenAddresses, (tokenAddress) => {
-      return {
-        target: tokenAddress,
-        params: poolAddress
-      }
+    _.each(pools, (pool) => {
+      _.each(tokensAddresses, (tokenAddress) => {
+        calls.push({
+          target: tokenAddress,
+          params: pool
+        })
+      });
     });
 
     let balanceOfResults = await sdk.api.abi.multiCall({
@@ -47,9 +51,12 @@
       }
     });
 
-    let symbolBalances = await sdk.api.util.toSymbols(balances);
+    for(let pool of pools) {
+      let balance = (await sdk.api.eth.getBalance({target: pool, block})).output;
+      balances['0x0000000000000000000000000000000000000000'] = BigNumber(balances['0x0000000000000000000000000000000000000000'] || 0).plus(balance).toFixed();
+    }
 
-    return symbolBalances.output;
+    return (await sdk.api.util.toSymbols(balances)).output;
   }
 
 /*==================================================
