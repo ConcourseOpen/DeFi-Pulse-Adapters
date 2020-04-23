89d54d24b48b05475d763a6f71bd2c30e2708fc0
arman37 <arman.it37@gmail.com>
2020-04-24 00:13:09 +0600

    Connext adapter translation

---
+++ projects/connext/index.js
@@ -0,0 +1,39 @@
+/*==================================================
+  Modules
+==================================================*/
+
+  const sdk = require('../../sdk');
+  const abi = require('./abi');
+
+/*==================================================
+  Settings
+==================================================*/
+
+  const hubAddress = '0xdfa6edAe2EC0cF1d4A60542422724A48195A5071';
+  const tokenDenominationAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+
+/*==================================================
+  Main
+==================================================*/
+
+  async function tvl (timestamp, block) {
+    const totalChannelToken = (await sdk.api.abi.call({
+      block,
+      target: hubAddress,
+      abi: abi['totalChannelToken'],
+    })).output;
+
+    return {[tokenDenominationAddress]: totalChannelToken};
+  }
+
+/*==================================================
+  Exports
+==================================================*/
+
+  module.exports = {
+    name: 'Connext',
+    token: null,
+    category: 'Lending',
+    start: 1551945204,  // Mar-07-2019 07:53:24 AM +UTC
+    tvl
+  };
