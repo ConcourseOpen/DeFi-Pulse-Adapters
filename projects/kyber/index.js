d0bfdca64cb73e7ee842ff479f27c473edfb142c
arman37 <arman.it37@gmail.com>
2020-04-24 00:22:30 +0600

    Adjust spaces

--- projects/kyber/index.js
+++ projects/kyber/index.js
@@ -2,70 +2,70 @@
   Modules
 ==================================================*/
 
-const sdk = require('../../sdk');
-const BigNumber = require('bignumber.js');
-const _ = require('underscore');
-const abi = require('./abi');
+  const sdk = require('../../sdk');
+  const BigNumber = require('bignumber.js');
+  const _ = require('underscore');
+  const abi = require('./abi');
 
 /*==================================================
   Main
 ==================================================*/
 
-async function tvl (timestamp, block) {
-  const balances = {};
+  async function tvl (timestamp, block) {
+    const balances = {};
 
-  /* pull kyber market addresses */
-  const reserveAddresses = (await sdk.api.abi.call({
-    target: abi['networkAddress'],
-    abi: abi['getReserves'],
-    block
-  })).output;
+    /* pull kyber market addresses */
+    const reserveAddresses = (await sdk.api.abi.call({
+      target: abi['networkAddress'],
+      abi: abi['getReserves'],
+      block
+    })).output;
 
-  const kyberTokens = (await sdk.api.util.kyberTokens()).output;
+    const kyberTokens = (await sdk.api.util.kyberTokens()).output;
 
-  let balanceOfCalls = [];
-  _.forEach(reserveAddresses, (reserveAddress) => {
-    balanceOfCalls = [
-      ...balanceOfCalls,
-      ..._.map(kyberTokens, (data, address) => ({
-        target: reserveAddress,
-        params: address
-      }))
-    ];
-  });
+    let balanceOfCalls = [];
+    _.forEach(reserveAddresses, (reserveAddress) => {
+      balanceOfCalls = [
+        ...balanceOfCalls,
+        ..._.map(kyberTokens, (data, address) => ({
+          target: reserveAddress,
+          params: address
+        }))
+      ];
+    });
 
-  const balanceOfResult = await sdk.api.abi.multiCall({
-    block,
-    calls: balanceOfCalls,
-    abi: abi['getBalance'],
-  });
+    const balanceOfResult = await sdk.api.abi.multiCall({
+      block,
+      calls: balanceOfCalls,
+      abi: abi['getBalance'],
+    });
 
-  /* combine token volumes on multiple markets */
-  _.forEach(balanceOfResult.output, (result) => {
-    let balance = new BigNumber(result.output || 0);
-    if (balance <= 0) return;
+    /* combine token volumes on multiple markets */
+    _.forEach(balanceOfResult.output, (result) => {
+      let balance = new BigNumber(result.output || 0);
+      if (balance <= 0) return;
 
-    let asset = result.input.params[0];
-    let total = balances[asset];
+      let asset = result.input.params[0];
+      let total = balances[asset];
 
-    if (total) {
-      balances[asset] = balance.plus(total).toFixed();
-    } else {
-      balances[asset] = balance.toFixed();
-    }
-  });
+      if (total) {
+        balances[asset] = balance.plus(total).toFixed();
+      } else {
+        balances[asset] = balance.toFixed();
+      }
+    });
 
-  return balances;
-}
+    return balances;
+  }
 
 /*==================================================
   Exports
 ==================================================*/
 
-module.exports = {
-  name: 'Kyber',
-  token: 'KNC',
-  category: 'DEXes',
-  start: 1546515458,  // Jan-03-2019 11:37:38 AM +UTC
-  tvl
-};
+  module.exports = {
+    name: 'Kyber',
+    token: 'KNC',
+    category: 'DEXes',
+    start: 1546515458,  // Jan-03-2019 11:37:38 AM +UTC
+    tvl,
+  };
