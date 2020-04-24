cc0b467c1e7f510a3270390a3633794dede490b4
Matthew Pilkey <m.pilkey@gmail.com>
2020-04-24 05:07:07 -0500

    standardize xdai

--- projects/xdai/index.js
+++ projects/xdai/index.js
@@ -3,53 +3,47 @@
   ==================================================*/
 
   const sdk = require('../../sdk');
+  const _ = require('underscore');
+  const BigNumber = require('bignumber.js');
+
+/*==================================================
+  Settings
+  ==================================================*/
+
+  const tokenAddresses = [
+    '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359', // SAI
+    '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+    '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215'  // CHAI
+  ];
 
 /*==================================================
   TVL
   ==================================================*/
 
   async function tvl(timestamp, block) {
-    const bridgeAddress = '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016';
-    const saiAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359';
-    const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-    const chaiAddress = '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215';
-    const daiDeploymentBlock = 8928158;
-    const initializeChaiBlock = 9779414;
-    const saiShutdownBlock = 9884448;
+    let balances = {};
 
-    let saiBalance = '0';
-    let daiBalance = '0';
-    let chaiBalance = '0';
+    const balanceOfResults = await sdk.api.abi.multiCall({
+      block,
+      calls: _.map(tokenAddresses, (address) => ({
+        target: address,
+        params: '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016'
+      })),
+      abi: 'erc20:balanceOf'
+    });
 
-    saiBalance = (await sdk.api.erc20.balanceOf({
-      target: saiAddress,
-      owner: bridgeAddress,
-      block: Math.min(block, saiShutdownBlock)
-    })).output;
+    _.each(balanceOfResults.output, (balanceOf) => {
+      if(balanceOf.success) {
+        let balance = balanceOf.output;
+        let address = balanceOf.input.target;
 
-    if (block >= daiDeploymentBlock) {
-      daiBalance = (await sdk.api.erc20.balanceOf({
-        target: daiAddress,
-        owner: bridgeAddress,
-        block
-      })).output;
-    }
+        if (BigNumber(balance).toNumber() <= 0) {
+          return;
+        }
 
-    // there is no sense in checking chai balance before chai contract was actually deployed
-    // and initializeChaiToken() was called on the bridge contract
-    if (block >= initializeChaiBlock) {
-      chaiBalance = (await sdk.api.erc20.balanceOf({
-        target: chaiAddress,
-        owner: bridgeAddress,
-        block
-      })).output;
-    }
-
-    const balances = {
-      [saiAddress]: saiBalance,
-      [daiAddress]: daiBalance,
-      [chaiAddress]: chaiBalance
-    };
+        balances[address] = BigNumber(balances[address] || 0).plus(balance).toFixed();
+      }
+    });
 
     const symbolBalances = await sdk.api.util.toSymbols(balances);
 
