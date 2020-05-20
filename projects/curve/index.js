7f79ac4733633c369fd5141af6baff4a28d7ce15
Matthew Pilkey <m.pilkey@gmail.com>
2020-05-19 22:46:42 -0500

    tested/curve update

--- projects/curve/index.js
+++ projects/curve/index.js
@@ -3,12 +3,38 @@
   ==================================================*/
 
   const sdk = require('../../sdk');
+  const _ = require('underscore');
+  const BigNumber = require('bignumber.js');
+
+/*==================================================
+  Settings
+  ==================================================*/
+
+  const yTokens = {
+    // yTokens curve.fi/y
+    '0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    '0xd6aD7a6750A7593E092a9B218d66C0A814a3436e': '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    '0x83f798e925BcD4017Eb265844FDDAbb448f1707D': '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    '0x73a052500105205d34Daf004eAb301916DA8190f': '0x0000000000085d4780B73119b644AE5ecd22b376',
+
+    // yTokens curve.fi/busd
+    '0xC2cB1040220768554cf699b0d863A3cd4324ce32': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    '0x26EA744E5B887E5205727f55dFBE8685e3b21951': '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    '0xE6354ed5bC4b393a5Aad09f21c46E101e692d447': '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    '0x04bC0Ab673d88aE9dbC9DA2380cB6B79C4BCa9aE': '0x4Fabb145d64652a948d72533023f6E7A623C7C53',
+
+    // ycTokens curve.fi/pax
+    '0x99d1Fa417f94dcD62BfE781a1213c092a47041Bc': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    '0x9777d7E2b60bB01759D0E2f8be2095df444cb07E': '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    '0x1bE5d71F2dA660BFdee8012dDc58D024448A0A59': '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+  }
 
 /*==================================================
   TVL
   ==================================================*/
 
   async function tvl(timestamp, block) {
+    let rawBalances = {};
     let balances = {};
 
     let swaps = [
@@ -18,13 +44,14 @@
       '0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27',
       '0xA5407eAE9Ba41422680e2e00537571bcC53efBfD',
       '0x06364f10B501e868329afBc005b3492902d6C763',
-    ]
+    ];
 
-    let coins = [2, 3, 4, 4, 4, 4]
+    let coins = [2, 3, 4, 4, 4, 4];
 
     let balancesCalls = swaps.flatMap((token, i) => {
       return Array.from(Array(coins[i]), (e, idx) =>({target: token, params: idx}))
-    })
+    });
+
     let balancesResults = await sdk.api.abi.multiCall({
       block,
       calls: balancesCalls,
@@ -46,12 +73,13 @@
         "payable": false,
         "type": "function",
         "gas": 2250
-      },
-    })
+      }
+    });
 
     let coinsCalls = swaps.flatMap((token, i) => {
       return Array.from(Array(coins[i]), (e, idx) =>({target: token, params: idx}))
-    })
+    });
+
     let coinsResults = await sdk.api.abi.multiCall({
       block,
       calls: coinsCalls,
@@ -74,40 +102,19 @@
         "type": "function",
         "gas": 2190
       },
-    })
+    });
 
     for(let [i, balance] of balancesResults.output.entries()) {
-      balances[coinsResults.output[i].output] = balance.output
+      if(balance.success) {
+        rawBalances[coinsResults.output[i].output] = balance.output
+      }
     }
 
-    return balances;
-  }
-
-  async function rates(timestamp, block) {
-    let yTokens = [
-      //yTokens curve.fi/y
-      '0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01',
-      '0xd6aD7a6750A7593E092a9B218d66C0A814a3436e',
-      '0x83f798e925BcD4017Eb265844FDDAbb448f1707D',
-      '0x73a052500105205d34Daf004eAb301916DA8190f',
-
-      //yTokens curve.fi/busd
-      '0xC2cB1040220768554cf699b0d863A3cd4324ce32',
-      '0x26EA744E5B887E5205727f55dFBE8685e3b21951',
-      '0xE6354ed5bC4b393a5Aad09f21c46E101e692d447',
-      '0x04bC0Ab673d88aE9dbC9DA2380cB6B79C4BCa9aE',
-
-      //ycTokens curve.fi/pax
-      '0x99d1Fa417f94dcD62BfE781a1213c092a47041Bc',
-      '0x9777d7E2b60bB01759D0E2f8be2095df444cb07E',
-      '0x1bE5d71F2dA660BFdee8012dDc58D024448A0A59',
-    ]
-
-    let yCalls = yTokens.map((token, i) => ({ target: token }))
-
-    let yRates = await sdk.api.abi.multiCall({
+    let yRates = (await sdk.api.abi.multiCall({
       block,
-      calls: yCalls,
+      calls: _.map(_.keys(yTokens), (token) => ({
+        target: token
+      })),
       abi:  {
         "constant":true,
         "inputs":[],
@@ -117,9 +124,20 @@
         "stateMutability":"view",
         "type":"function"
       }
-    })
-  }
+    })).output;
 
+    _.each(rawBalances, (balance, address) => {
+        if(yTokens[address]) {
+          let rate = _.find(yRates, (call) => call.input.target == address);
+          balance = BigNumber(rate.output).div(1e18).times(balance).toFixed();
+          address = yTokens[address];
+        }
+
+        balances[address] = BigNumber(balances[address] || 0).plus(balance).toFixed();
+    });
+
+    return balances;
+  }
 
 /*==================================================
   Exports
@@ -129,6 +147,6 @@
     name: 'Curve Finance',
     token: null,
     category: 'dexes',
-    start: 1582609711,        // 25/02/2020 @ 5.48am UTC
+    start: 1582588800, // 02/25/2020 @ 12:00am (UTC)
     tvl
   }
