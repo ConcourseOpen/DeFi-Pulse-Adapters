bda5a836e190d698b2160a42b8c5aa309d0f43a4
Matthew Pilkey <m.pilkey@gmail.com>
2020-06-09 07:45:30 -0500

    curve historical update

--- projects/curve/index.js
+++ projects/curve/index.js
@@ -11,6 +11,8 @@
   ==================================================*/
 
   const yTokens = {
+    '0xF61718057901F84C4eEC4339EF8f0D86D2B45600': '0x57Ab1ec28D129707052df4dF418D58a2D46d5f51',
+
     // yTokens curve.fi/y
     '0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
     '0xd6aD7a6750A7593E092a9B218d66C0A814a3436e': '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
@@ -38,19 +40,24 @@
     let balances = {};
 
     let swaps = [
+      '0xe5FdBab9Ad428bBB469Dee4CB6608C0a8895CbA5',
+      '0x2e60CF74d81ac34eB21eEff58Db4D385920ef419',
       '0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56',
       '0x52EA46506B9CC5Ef470C5bf89f17Dc28bB35D85C',
       '0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51',
       '0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27',
+      '0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966',
       '0xA5407eAE9Ba41422680e2e00537571bcC53efBfD',
       '0x06364f10B501e868329afBc005b3492902d6C763',
-    ];
+    ]
 
-    let coins = [2, 3, 4, 4, 4, 4];
+    let coins = [2, 2, 2, 3, 4, 4, 2, 4, 4]
 
     let balancesCalls = swaps.flatMap((token, i) => {
       return Array.from(Array(coins[i]), (e, idx) =>({target: token, params: idx}))
     });
+
+    balancesCalls = balancesCalls.filter(call => !(call.target == '0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966' && call.params == 1));
 
     let balancesResults = await sdk.api.abi.multiCall({
       block,
@@ -80,6 +87,8 @@
       return Array.from(Array(coins[i]), (e, idx) =>({target: token, params: idx}))
     });
 
+    coinsCalls = coinsCalls.filter(call => !(call.target == '0xeDf54bC005bc2Df0Cc6A675596e843D28b16A966' && call.params == 1))
+
     let coinsResults = await sdk.api.abi.multiCall({
       block,
       calls: coinsCalls,
@@ -101,7 +110,7 @@
         "payable": false,
         "type": "function",
         "gas": 2190
-      },
+      }
     });
 
     for(let [i, balance] of balancesResults.output.entries()) {
@@ -127,17 +136,18 @@
     })).output;
 
     _.each(rawBalances, (balance, address) => {
-        if(yTokens[address]) {
-          let rate = _.find(yRates, (call) => call.input.target == address);
-          balance = BigNumber(rate.output).div(1e18).times(balance).toFixed();
-          address = yTokens[address];
-        }
+      if(yTokens[address]) {
+        let rate = _.find(yRates, (call) => call.input.target == address);
+        balance = BigNumber(rate.output).div(1e18).times(balance).toFixed();
+        address = yTokens[address];
+      }
 
-        balances[address] = BigNumber(balances[address] || 0).plus(balance).toFixed();
+      balances[address] = BigNumber(balances[address] || 0).plus(balance).toFixed();
     });
 
     return balances;
   }
+
 
 /*==================================================
   Exports
@@ -147,6 +157,6 @@
     name: 'Curve Finance',
     token: null,
     category: 'dexes',
-    start: 1582588800, // 02/25/2020 @ 12:00am (UTC)
+    start: 1578009600, // 01/04/2020 @ 12:00am (UTC)
     tvl
   }
