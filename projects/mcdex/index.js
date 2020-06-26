97c76009ca6bf2ce34fccaafb23a6e38ad7b2d78
arman37 <arman.it37@gmail.com>
2020-06-26 15:21:50 +0600

    Small refactoring

--- projects/mcdex/index.js
+++ projects/mcdex/index.js
@@ -9,10 +9,11 @@
   ==================================================*/
 
   async function tvl(timestamp, block) {
-    let balances = {
-      "0x0000000000000000000000000000000000000000": (await sdk.api.eth.getBalance({ target: '0x220a9f0DD581cbc58fcFb907De0454cBF3777f76', block })).output,
+    const ethBalance = (await sdk.api.eth.getBalance({ target: '0x220a9f0DD581cbc58fcFb907De0454cBF3777f76', block })).output;
+
+    return {
+      "0x0000000000000000000000000000000000000000": ethBalance,
     };
-    return balances;
   }
 
 /*==================================================
@@ -25,5 +26,5 @@
     token: 'MCB',
     category: 'derivatives',  // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
     start: 1592478136,        // block 10289316
-    tvl
-  }
+    tvl,
+  };
