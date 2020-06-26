68759033e0ed7a8c0384c5d93dc6f6570c4f4aec
arman37 <arman.it37@gmail.com>
2020-06-26 15:25:18 +0600

    Convert address to symbol

--- projects/mcdex/index.js
+++ projects/mcdex/index.js
@@ -11,9 +11,9 @@
   async function tvl(timestamp, block) {
     const ethBalance = (await sdk.api.eth.getBalance({ target: '0x220a9f0DD581cbc58fcFb907De0454cBF3777f76', block })).output;
 
-    return {
+    return (await sdk.api.util.toSymbols({
       "0x0000000000000000000000000000000000000000": ethBalance,
-    };
+    })).output;
   }
 
 /*==================================================
@@ -24,7 +24,7 @@
     name: 'MCDEX',
     website: 'https://mcdex.io',
     token: 'MCB',
-    category: 'derivatives',  // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
+    category: 'Derivatives',  // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
     start: 1592478136,        // block 10289316
     tvl,
   };
