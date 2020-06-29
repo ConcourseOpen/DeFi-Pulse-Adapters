97cfdf4cb8bd75c525c7e2a8c34fd61c7828c2e1
Noah Zinsmeister <noahwz@gmail.com>
2020-06-29 15:38:23 -0400

    use sdk.api.util.tokenList

--- projects/uniswap/utils.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-module.exports = {
-  getSupportedTokens: async () => {
-    const tokens = await axios.get('https://public.defipulse.com/api/TokenList')
-    return tokens.data.map((token) => token.contract.toLowerCase())
-  },
-}
