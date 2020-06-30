c00bb6cebd7ae7b87e5170a503d0171af0926057
arman37 <arman.it37@gmail.com>
2020-07-01 04:50:04 +0600

    Minor refactoring

--- projects/uniswap/v1.js
+++ projects/uniswap/v1.js
@@ -1,56 +1,60 @@
-const BigNumber = require('bignumber.js')
+const sdk = require('../../sdk');
+const BigNumber = require('bignumber.js');
 
-const sdk = require('../../sdk')
+const START_BLOCK = 6627917;
+const FACTORY = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95';
+const ETH = '0x0000000000000000000000000000000000000000'.toLowerCase();
 
-const START_BLOCK = 6627917
-const FACTORY = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
-const ETH = '0x0000000000000000000000000000000000000000'.toLowerCase()
+module.exports = async function tvl(timestamp, block) {
+  const supportedTokens = await (
+    sdk
+      .api
+      .util
+      .tokenList()
+      .then((supportedTokens) => supportedTokens.map(({ contract }) => contract))
+  );
 
-module.exports = async function tvl(_, block) {
-  const supportedTokens = await sdk.api.util
-    .tokenList()
-    .then((supportedTokens) => supportedTokens.map(({ contract }) => contract))
-
-  const logs = await sdk.api.util
+  const logs = (await sdk.api.util
     .getLogs({
       keys: [],
       toBlock: block,
       target: FACTORY,
       fromBlock: START_BLOCK,
       topic: 'NewExchange(address,address)',
-    })
-    .then(({ output }) => output)
+    })).output;
 
-  const exchanges = {}
+  const exchanges = {};
   logs.forEach((log) => {
-    const tokenAddress = `0x${log.topics[1].substring(26)}`.toLowerCase()
+    const tokenAddress = `0x${log.topics[1].substring(26)}`.toLowerCase();
 
     // only consider supported tokens
     if (supportedTokens.includes(tokenAddress)) {
-      const exchangeAddress = `0x${log.topics[2].substring(26)}`.toLowerCase()
-      exchanges[exchangeAddress] = tokenAddress
+      const exchangeAddress = `0x${log.topics[2].substring(26)}`.toLowerCase();
+      exchanges[exchangeAddress] = tokenAddress;
     }
-  })
+  });
 
-  const tokenBalances = await sdk.api.abi
-    .multiCall({
-      abi: 'erc20:balanceOf',
-      calls: Object.keys(exchanges).map((exchangeAddress) => ({
-        target: exchanges[exchangeAddress],
-        params: exchangeAddress,
-      })),
-      block,
-    })
-    .then(({ output }) => output)
+  const tokenBalances = (
+    await sdk.api.abi
+      .multiCall({
+        abi: 'erc20:balanceOf',
+        calls: Object.keys(exchanges).map((exchangeAddress) => ({
+          target: exchanges[exchangeAddress],
+          params: exchangeAddress,
+        })),
+        block,
+      })
+  ).output;
 
   // note that this undercounts ETH locked
   // it only measures ETH in exchanges for supported tokens
-  const ETHBalances = await sdk.api.eth
-    .getBalances({
-      targets: Object.keys(exchanges).map((exchangeAddress) => exchangeAddress),
-      block,
-    })
-    .then(({ output }) => output)
+  const ETHBalances = (
+    await sdk.api.eth
+      .getBalances({
+        targets: Object.keys(exchanges).map((exchangeAddress) => exchangeAddress),
+        block,
+      })
+  ).output;
 
   return tokenBalances.reduce(
     (accumulator, tokenBalance) => {
@@ -71,4 +75,4 @@ module.exports = async function tvl(_, block) {
       ).toFixed(),
     }
   )
-}
+};
--- projects/uniswap/v2.js
+++ projects/uniswap/v2.js
@@ -1,27 +1,32 @@
-const BigNumber = require('bignumber.js')
+const BigNumber = require('bignumber.js');
 
-const sdk = require('../../sdk')
-const token0 = require('./abis/token0.json')
-const token1 = require('./abis/token1.json')
-const getReserves = require('./abis/getReserves.json')
+const sdk = require('../../sdk');
+const token0 = require('./abis/token0.json');
+const token1 = require('./abis/token1.json');
+const getReserves = require('./abis/getReserves.json');
 
-const START_BLOCK = 10000835
-const FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+const START_BLOCK = 10000835;
+const FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 
 module.exports = async function tvl(_, block) {
-  const supportedTokens = await sdk.api.util
-    .tokenList()
-    .then((supportedTokens) => supportedTokens.map(({ contract }) => contract))
+  const supportedTokens = await (
+    sdk
+      .api
+      .util
+      .tokenList()
+      .then((supportedTokens) => supportedTokens.map(({ contract }) => contract))
+  );
 
-  const logs = await sdk.api.util
-    .getLogs({
-      keys: [],
-      toBlock: block,
-      target: FACTORY,
-      fromBlock: START_BLOCK,
-      topic: 'PairCreated(address,address,address,uint256)',
-    })
-    .then(({ output }) => output)
+  const logs = (
+    await sdk.api.util
+      .getLogs({
+        keys: [],
+        toBlock: block,
+        target: FACTORY,
+        fromBlock: START_BLOCK,
+        topic: 'PairCreated(address,address,address,uint256)',
+      })
+  ).output;
 
   const pairAddresses = logs
     // sometimes the full log is emitted
@@ -29,7 +34,7 @@ module.exports = async function tvl(_, block) {
       typeof log === 'string' ? log : `0x${log.data.slice(64 - 40 + 2, 64 + 2)}`
     )
     // lowercase
-    .map((pairAddress) => pairAddress.toLowerCase())
+    .map((pairAddress) => pairAddress.toLowerCase());
 
   const [token0Addresses, token1Addresses] = await Promise.all([
     sdk.api.abi
@@ -50,57 +55,59 @@ module.exports = async function tvl(_, block) {
         block,
       })
       .then(({ output }) => output),
-  ])
+  ]);
 
-  const pairs = {}
+  const pairs = {};
   // add token0Addresses
   token0Addresses.forEach((token0Address) => {
     if (token0Address.success) {
-      const tokenAddress = token0Address.output.toLowerCase()
+      const tokenAddress = token0Address.output.toLowerCase();
+
       if (supportedTokens.includes(tokenAddress)) {
-        const pairAddress = token0Address.input.target.toLowerCase()
+        const pairAddress = token0Address.input.target.toLowerCase();
         pairs[pairAddress] = {
           token0Address: tokenAddress,
         }
       }
     }
-  })
+  });
+
   // add token1Addresses
   token1Addresses.forEach((token1Address) => {
     if (token1Address.success) {
-      const tokenAddress = token1Address.output.toLowerCase()
+      const tokenAddress = token1Address.output.toLowerCase();
       if (supportedTokens.includes(tokenAddress)) {
-        const pairAddress = token1Address.input.target.toLowerCase()
+        const pairAddress = token1Address.input.target.toLowerCase();
         pairs[pairAddress] = {
           ...(pairs[pairAddress] || {}),
           token1Address: tokenAddress,
         }
       }
     }
-  })
+  });
 
-  const reserves = await sdk.api.abi
+  const reserves = (await sdk.api.abi
     .multiCall({
       abi: getReserves,
       calls: Object.keys(pairs).map((pairAddress) => ({
         target: pairAddress,
       })),
       block,
-    })
-    .then(({ output }) => output)
+    })).output;
 
   return reserves.reduce((accumulator, reserve, i) => {
     if (reserve.success) {
-      const pairAddress = reserve.input.target.toLowerCase()
-      const pair = pairs[pairAddress] || {}
+      const pairAddress = reserve.input.target.toLowerCase();
+      const pair = pairs[pairAddress] || {};
 
       // handle reserve0
       if (pair.token0Address) {
-        const reserve0 = new BigNumber(reserve.output['0'])
+        const reserve0 = new BigNumber(reserve.output['0']);
         if (!reserve0.isZero()) {
           const existingBalance = new BigNumber(
             accumulator[pair.token0Address] || '0'
-          )
+          );
+
           accumulator[pair.token0Address] = existingBalance
             .plus(reserve0)
             .toFixed()
@@ -109,11 +116,13 @@ module.exports = async function tvl(_, block) {
 
       // handle reserve1
       if (pair.token1Address) {
-        const reserve1 = new BigNumber(reserve.output['1'])
+        const reserve1 = new BigNumber(reserve.output['1']);
+
         if (!reserve1.isZero()) {
           const existingBalance = new BigNumber(
             accumulator[pair.token1Address] || '0'
-          )
+          );
+
           accumulator[pair.token1Address] = existingBalance
             .plus(reserve1)
             .toFixed()
@@ -123,4 +132,4 @@ module.exports = async function tvl(_, block) {
 
     return accumulator
   }, {})
-}
+};
