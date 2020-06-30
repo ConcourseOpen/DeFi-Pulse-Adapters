91f95e21bce757fe93443852f7eb81aa1d65e751
arman37 <arman.it37@gmail.com>
2020-07-01 04:50:04 +0600

    Minor refactoring

--- projects/uniswap/index.js
+++ projects/uniswap/index.js
@@ -1,26 +1,31 @@
 const BigNumber = require('bignumber.js')
 
-const v1TVL = require('./v1')
-const v2TVL = require('./v2')
+const v1TVL = require('./v1');
+const v2TVL = require('./v2');
 
-const ETH = '0x0000000000000000000000000000000000000000'.toLowerCase()
-const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'.toLowerCase()
+const ETH = '0x0000000000000000000000000000000000000000'.toLowerCase();
+const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'.toLowerCase();
 
-async function tvl(_, block) {
-  const [v1, v2] = await Promise.all([v1TVL(_, block), v2TVL(_, block)])
+async function tvl(timestamp, block) {
+  const [v1, v2] = await Promise.all([v1TVL(timestamp, block), v2TVL(timestamp, block)]);
 
   // replace WETH with ETH for v2
-  v2[ETH] = v2[WETH]
-  delete v2[WETH]
+  v2[ETH] = v2[WETH];
+  delete v2[WETH];
 
-  const tokenAddresses = new Set(Object.keys(v1).concat(Object.keys(v2)))
+  const tokenAddresses = new Set(Object.keys(v1).concat(Object.keys(v2)));
 
-  return Array.from(tokenAddresses).reduce((accumulator, tokenAddress) => {
-    const v1Balance = new BigNumber(v1[tokenAddress] || '0')
-    const v2Balance = new BigNumber(v2[tokenAddress] || '0')
-    accumulator[tokenAddress] = v1Balance.plus(v2Balance).toFixed()
-    return accumulator
-  }, {})
+  return (
+    Array
+      .from(tokenAddresses)
+      .reduce((accumulator, tokenAddress) => {
+        const v1Balance = new BigNumber(v1[tokenAddress] || '0');
+        const v2Balance = new BigNumber(v2[tokenAddress] || '0');
+        accumulator[tokenAddress] = v1Balance.plus(v2Balance).toFixed();
+
+        return accumulator
+      }, {})
+  );
 }
 
 module.exports = {
@@ -29,4 +34,4 @@ module.exports = {
   category: 'dexes',
   start: 1541116800, // 11/02/2018 @ 12:00am (UTC)
   tvl,
-}
+};
