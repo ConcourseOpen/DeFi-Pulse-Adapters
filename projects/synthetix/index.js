9f5d33ad8f46e47cf8c8f277635b3383ad25dbfa
John L <penandlim@gmail.com>
2020-07-27 16:45:51 -0700

    Synthetix account number limit increased 1000 -> 10000
    * Uses graph protocol to get more accounts.

--- projects/synthetix/index.js
+++ projects/synthetix/index.js
@@ -5,8 +5,8 @@
   const sdk = require('../../sdk');
   const BigNumber = require('bignumber.js');
   const _ = require('underscore');
-  const axios = require('axios');
   const abi = require('./abi');
+  const pageResults = require('graph-results-pager');
 
 /*==================================================
   Settings
@@ -14,40 +14,61 @@
 
   const snxContract = '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F';
   const stateAddress = '0x4b9Ca5607f1fF8019c1C6A3c2f0CC8de622D5B82';
+  const snxGraphEndpoint = 'https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix';
 
 /*==================================================
   Main
 ==================================================*/
 
   async function tvl (timestamp, block) {
+
     let totalSNXLocked = new BigNumber(0);
-    const holders = (await axios(`https://api.ethplorer.io/getTopTokenHolders/${snxContract}?apiKey=uxips4087TKQO109&limit=1000`)).data["holders"].map(holder => holder.address);
+
+    const holders = await SNXHolders(block);
+
     const issuanceRatio = (await sdk.api.abi.call({
       block,
       target: stateAddress,
       abi: abi['issuanceRatio']
     })).output;
 
-    const collateral = (await sdk.api.abi.multiCall({
-      block,
-      abi: abi['collateral'],
-      calls: _.map(holders, holder => ({ target: snxContract, params: holder }))
-    })).output;
-
     const ratio = (await sdk.api.abi.multiCall({
       block,
       abi: abi['collateralisationRatio'],
-      calls: _.map(holders, holder => ({ target: snxContract, params: holder }))
+      calls: _.map(holders, holder => ({ target: snxContract, params: holder.id }))
     })).output;
 
     _.forEach(holders, (holder) => {
-      let _collateral = _.find(collateral, result => result.input.params[0] === holder).output;
-      let _ratio = _.find(ratio, result => result.input.params[0] === holder).output;
+      let _collateral = holder.collateral;
+      let _ratio = _.find(ratio, result => result.input.params[0] === holder.id).output;
       let locked = _collateral * Math.min(1, _ratio / issuanceRatio);
       totalSNXLocked = totalSNXLocked.plus(locked);
     });
 
     return { [snxContract]: totalSNXLocked.toFixed() };
+  }
+
+  // Uses graph protocol to run through SNX contract. Since there is a limit of 100 results per query
+  // we can use graph-results-pager library to increase the limit.
+  async function SNXHolders(blockNumber) {
+    return await pageResults({
+      api: snxGraphEndpoint,
+      query: {
+        entity: 'snxholders',
+        selection: {
+          orderBy: 'collateral',
+          orderDirection: 'desc',
+          block : {
+            number : blockNumber
+          },
+          where : {
+            collateral_gt : 0
+          }
+        },
+        properties: ['collateral', 'id'],
+      },
+      max: 10000, // top 10000 SNX holders with collateral. At the time of this commit, there are 51,309 SNX holders. (7/27/2020)
+    });
   }
 
 /*==================================================
@@ -57,7 +78,7 @@
   module.exports = {
     name: 'Synthetix',
     token: 'SNX',
-    category: 'Derivatives',
+    category: 'derivatives',
     start: 1565287200,  // Fri Aug 09 2019 00:00:00
     tvl,
   };
