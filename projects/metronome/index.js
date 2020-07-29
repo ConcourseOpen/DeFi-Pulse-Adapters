e9b40904741dd2901c35b6a0e059c88cd190d316
arman37 <arman.it37@gmail.com>
2020-07-30 00:25:01 +0600

    Metronome adapter cleanup

--- projects/metronome/index.js
+++ projects/metronome/index.js
@@ -13,23 +13,30 @@
     const acc = '0x686e5ac50D9236A9b7406791256e47feDDB26AbA';
     const met = '0xa3d58c4E56fedCae3a7c43A725aeE9A71F0ece4e';
     const proceeds = '0x68c4b7d05fae45bcb6192bb93e246c77e98360e1';
+
     const [accBalance, proceedsBalance] = await Promise.all([
       sdk.api.eth.getBalance({ target: acc, block }),
       sdk.api.eth.getBalance({ target: proceeds, block })
     ]);
+
     const ethBalance = ((BigNumber(accBalance.output || 0))
       .plus(BigNumber(proceedsBalance.output || 0)))
       .toFixed();
-    let metBalance = (await sdk.api.abi.multiCall({
+
+    let metBalance = (await sdk.api.abi.call({
       block,
-      calls: [{ target: met, params: acc}],
+      target: met,
+      params: acc,
       abi: 'erc20:balanceOf'
-    }))
+    })).output;
+
     let balances = {
-      "0x0000000000000000000000000000000000000000": ethBalance
-    }
+      [met]: metBalance,
+      "0x0000000000000000000000000000000000000000": ethBalance,
+    };
     sdk.util.sumMultiBalanceOf(balances, metBalance);
-    return balances
+
+    return balances;
   }
 
 /*==================================================
