e368f3bb4fa9afe711953922c8a045e51b115f3c
guosanbin <guosanbin@theforceprotocol.com>
2020-08-12 12:26:24 +0800

    add fortube tvl

---
+++ projects/fortube/index.js
@@ -0,0 +1,90 @@
+/*==================================================
+  Modules
+  ==================================================*/
+
+const sdk = require("../../sdk");
+const _ = require("underscore");
+const abi = require("./abi.json");
+
+/*==================================================
+    Settings
+    ==================================================*/
+const ForTube = "0xE48BC2Ba0F2d2E140382d8B5C8f261a3d35Ed09C";
+const EthAddress = "0x0000000000000000000000000000000000000000";
+
+async function getCollateralMarketsLength(block) {
+    return (
+        await sdk.api.abi.call({
+            block,
+            target: ForTube,
+            params: [],
+            abi: abi["getCollateralMarketsLength"],
+        })
+    ).output;
+}
+
+// returns {asset: {symbol, decimals}}
+async function getErc20Assets(block) {
+    let erc20Assets = [];
+    let calls = [];
+    let length = await getCollateralMarketsLength(block);
+    for (var i = 0; i < length; i++) {
+        calls.push({
+            target: ForTube,
+            params: i,
+        });
+    }
+    let erc20AssetResults = await sdk.api.abi.multiCall({
+        block,
+        calls: calls,
+        abi: abi["collateralTokens"],
+    });
+    _.each(erc20AssetResults.output, (result) => {
+        if (result.success && result.output != EthAddress) {
+            erc20Assets.push(result.output);
+        }
+    });
+
+    console.log("erc20Assets: ", erc20Assets);
+    return erc20Assets;
+}
+
+/*==================================================
+    TVL
+    ==================================================*/
+async function tvl(timestamp, block) {
+    let balances = {};
+    let erc20Assets = await getErc20Assets(block);
+
+    // Get erc20 assets locked
+    let balanceOfResults = await sdk.api.abi.multiCall({
+        block,
+        calls: _.map(erc20Assets, (asset) => ({
+            target: asset,
+            params: ForTube,
+        })),
+        abi: "erc20:balanceOf",
+    });
+
+    //add ETH tvl
+    balances[EthAddress] = (
+        await sdk.api.eth.getBalance({ target: ForTube, block })
+    ).output;
+
+    sdk.util.sumMultiBalanceOf(balances, balanceOfResults);
+
+    console.log("balances: ", balances);
+    return balances;
+}
+
+/*==================================================
+    Exports
+    ==================================================*/
+
+module.exports = {
+    name: "ForTube",
+    token: null,
+    category: "Lending",
+    start: 1596294236, // Aug-01-2020 03:03:56 PM +UTC
+    tvl,
+};
