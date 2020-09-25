6ac32613118af0f3ecfe2ca72aa3111ce9d096b7
thiagomva <thiagomvaraujo@gmail.com>
2020-09-25 15:58:01 -0300

    Include ACO Pools balances

--- projects/aco/index.js
+++ projects/aco/index.js
@@ -9,14 +9,23 @@ const { default: BigNumber } = require('bignumber.js');
 
   const START_BLOCK = 10104891;
   const FACTORY = '0x176b98ab38d1aE8fF3F30bF07f9B93E26F559C17';  
+  const POOLS_FACTORY = '0x4db1d076ed0a3f2bb9b105d96edfb1671916f6ca';  
+  const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';  
 
 /*==================================================
   TVL
   ==================================================*/
   
-  const getTokenAddressFromLogData = data => '0x' + data.substring(154, 194);
+  const getTokenAddressFromNewAcoTokenLogData = data => '0x' + data.substring(154, 194);
+
+  const getPoolAddressFromNewAcoPoolLogData = data => '0x' + data.substring(410, 450);
+
+  const getUnderlyingAddressFromNewAcoPoolLogData = data => '0x' + data.substring(26, 66);
+  const getStrikeAssetAddressFromNewAcoPoolLogData = data => '0x' + data.substring(26, 66);
 
   async function tvl(timestamp, block) {
+    let result = await sdk.api.util.tokenList();
+    
     const logs = (await sdk.api.util
       .getLogs({
         keys: [],
@@ -28,7 +37,7 @@ const { default: BigNumber } = require('bignumber.js');
 
     let acoOptionsAddresses = [];
     logs.forEach((log) => {
-      const address = getTokenAddressFromLogData(log.data);
+      const address = getTokenAddressFromNewAcoTokenLogData(log.data);
       acoOptionsAddresses.push(address)
     });
     
@@ -56,11 +65,12 @@ const { default: BigNumber } = require('bignumber.js');
       abi: abi.totalCollateral,
     });
 
-    let balances = {"0x0000000000000000000000000000000000000000": "0"}
+    let balances = {}
+    balances[ETHER_ADDRESS] = "0"
 
     _.each(totalCollateralResult.output, (result) => {
       if(result.success) {
-        var colateralAddress = collateralAddressMap[result.input.target]
+        var colateralAddress = collateralAddressMap[result.input.target].toLowerCase()
         if (!balances[colateralAddress]) {
           balances[colateralAddress] = "0"
         }
@@ -68,6 +78,50 @@ const { default: BigNumber } = require('bignumber.js');
         balances[colateralAddress] = existingBalance.plus(new BigNumber(result.output)).toFixed()
       }
     });
+
+    const newAcoPoolLogs = (await sdk.api.util
+      .getLogs({
+        keys: [],
+        toBlock: block,
+        target: POOLS_FACTORY,
+        fromBlock: START_BLOCK,
+        topic: 'NewAcoPool(address,address,bool,uint256,uint256,uint256,uint256,uint256,bool,address,address)',
+      })).output;
+
+    let acoPools = {};
+    newAcoPoolLogs.forEach((log) => {
+      const address = getPoolAddressFromNewAcoPoolLogData(log.data).toLowerCase();
+      const underlyingAddress = getUnderlyingAddressFromNewAcoPoolLogData(log.topics[1]).toLowerCase();
+      const strikeAssetAddress = getStrikeAssetAddressFromNewAcoPoolLogData(log.topics[2]).toLowerCase();
+      acoPools[address] = {underlying: underlyingAddress, strikeAsset: strikeAssetAddress}
+    });
+
+    let poolCallsMap = _.map(acoPools, (poolData, poolAddress) => ({
+      target: poolData.underlying,
+      params: poolAddress,
+    }))
+
+    poolCallsMap = poolCallsMap.concat(_.map(acoPools, (poolData, poolAddress) => ({
+      target: poolData.strikeAsset,
+      params: poolAddress,
+    })))
+
+    let erc20CallsMap = poolCallsMap.filter((f) => f.target !== ETHER_ADDRESS)
+
+    let poolBalances = await sdk.api.abi.multiCall({
+      block,
+      calls: erc20CallsMap,
+      abi: 'erc20:balanceOf',
+    });
+    sdk.util.sumMultiBalanceOf(balances, poolBalances);
+
+    let ethCallsMap = poolCallsMap.filter((f) => f.target === ETHER_ADDRESS)
+    await (
+      Promise.all(ethCallsMap.map(async (ethCall) => {
+        const balance = (await sdk.api.eth.getBalance({target: ethCall.params, block})).output;
+        balances[ETHER_ADDRESS] = BigNumber(balances[ETHER_ADDRESS]).plus(new BigNumber(balance)).toFixed();
+      }))
+    );
 
     return balances;
   }
