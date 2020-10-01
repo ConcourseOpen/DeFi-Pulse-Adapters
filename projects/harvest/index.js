f5ccb03f7b15d63f221a980123eafc009d3f286b
arman37 <arman.it37@gmail.com>
2020-10-02 02:07:56 +0600

    Optimise harvest adapter

--- projects/harvest/index.js
+++ projects/harvest/index.js
@@ -3,6 +3,7 @@
   ==================================================*/
 
   const sdk = require('../../sdk');
+  const abi = require('./abi.json');
 
 /*==================================================
   TVL
@@ -17,20 +18,28 @@
     'fRENBTC':     {underlying: 'RENBTC', decimals: 8, contract: '0xfBe122D0ba3c75e1F7C80bd27613c9f35B81FEeC', created: 10802986 },
     'fCRVRENWBTC': {underlying: 'CRVRENWBTC', decimals: 18, contract: '0x192E9d29D43db385063799BC239E772c3b6888F3', created: 10815917 },
   };
-  
-  const fABISharePrice = {"constant":true,"inputs":[],"name":"getPricePerFullShare","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"};
-  const fABIUnderlyingUnit = {"constant":true,"inputs":[],"name":"underlyingUnit","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"};
 
   async function tvl(timestamp, block) {
+    const promises = [
+      getUnderlying('fWETH',block),
+      getUnderlying('fDAI',block),
+      getUnderlying('fUSDC',block),
+      getUnderlying('fUSDT',block),
+      getUnderlying('fWBTC',block),
+      getUnderlying('fRENBTC',block),
+    ];
+
+    let results = await Promise.all(promises);
+
 
     let balances = {
-      '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': (await getUnderlying('fWETH',block)),   // WETH
-      '0x6B175474E89094C44Da98b954EedeAC495271d0F': (await getUnderlying('fDAI',block)),    // DAI
-      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': (await getUnderlying('fUSDC',block)),   // USDC
-      '0xdAC17F958D2ee523a2206206994597C13D831ec7': (await getUnderlying('fUSDT',block)),   // USDT
-      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': (await getUnderlying('fWBTC',block)),   // WBTC
-      '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D': Math.trunc(await getUnderlying('fRENBTC',block))  // RENBTC
-                                                      + Math.trunc(await getUnderlying('fRENBTC',block)), //crvRENWBTC, estimate
+      '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': results[0],   // WETH
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F': results[1],    // DAI
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': results[2],   // USDC
+      '0xdAC17F958D2ee523a2206206994597C13D831ec7': results[3],   // USDT
+      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': results[4],   // WBTC
+      '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D': Math.trunc(results[5])  // RENBTC
+                                                      + Math.trunc(results[5]), //crvRENWBTC, estimate
       // TODO Uniswap LP underlying
     };
 
@@ -39,11 +48,21 @@
 
   async function getUnderlying(token, block) {
     if (block > fTokens[token].created) {
-      let fBalance = (await sdk.api.abi.call({block, target: fTokens[token].contract, abi: 'erc20:totalSupply', })).output;
-      let fSharePrice = (await sdk.api.abi.call({block, target: fTokens[token].contract, abi: fABISharePrice, })).output;
-      let fUnderlyingUnit = (await sdk.api.abi.call({block, target: fTokens[token].contract, abi: fABIUnderlyingUnit, })).output;
+      const promises = [
+        sdk.api.abi.call({ block, target: fTokens[token].contract, abi: 'erc20:totalSupply', }),
+        sdk.api.abi.call({ block, target: fTokens[token].contract, abi: abi['fABISharePrice'], }),
+        sdk.api.abi.call({ block, target: fTokens[token].contract, abi: abi['fABIUnderlyingUnit'], })
+      ];
+
+      let results = await Promise.all(promises);
+
+      let fBalance = results[0].output;
+      let fSharePrice = results[1].output;
+      let fUnderlyingUnit = results[2].output;
+
       return fBalance * fSharePrice / fUnderlyingUnit;
     }
+
     return 0;
 }
 
@@ -58,4 +77,4 @@
     category: 'assets',       // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
     start: 1598893200,        // unix timestamp (utc 0) specifying when the project began, or where live data begins
     tvl                       // tvl adapter
-  }
+  };
