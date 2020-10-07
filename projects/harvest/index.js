b1b0d86329690d2f5a802e279c7f4bed01c40283
Silage Pete <silagepete@protonmail.com>
2020-10-07 01:09:44 +0000

    [Harvest] add fTUSD

--- projects/harvest/index.js
+++ projects/harvest/index.js
@@ -13,6 +13,7 @@
     'fWETH':       {underlying: 'WETH', decimals: 18, contract: '0x8e298734681adbfC41ee5d17FF8B0d6d803e7098', created: 10861886 },
     'fUSDC':       {underlying: 'USDC', decimals: 6,  contract: '0xc3F7ffb5d5869B3ade9448D094d81B0521e8326f', created: 10770105 },
     'fUSDT':       {underlying: 'USDT', decimals: 6,  contract: '0xc7EE21406BB581e741FBb8B21f213188433D9f2F', created: 10770108 },
+    'fTUSD':       {underlying: 'TUSD', decimals: 18, contract: '0x7674622c63Bee7F46E86a4A5A18976693D54441b', created: 10997721 },
     'fDAI':        {underlying: 'DAI',  decimals: 18, contract: '0xe85C8581e60D7Cd32Bbfd86303d2A4FA6a951Dac', created: 10770103 },
     'fWBTC':       {underlying: 'WBTC', decimals: 8,  contract: '0xc07EB91961662D275E2D285BdC21885A4Db136B0', created: 10802976 },
     'fRENBTC':     {underlying: 'RENBTC', decimals: 8, contract: '0xfBe122D0ba3c75e1F7C80bd27613c9f35B81FEeC', created: 10802986 },
@@ -25,6 +26,7 @@
       getUnderlying('fDAI',block),
       getUnderlying('fUSDC',block),
       getUnderlying('fUSDT',block),
+      getUnderlying('fTUSD',block),
       getUnderlying('fWBTC',block),
       getUnderlying('fRENBTC',block),
     ];
@@ -34,12 +36,13 @@
 
     let balances = {
       '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': results[0],   // WETH
-      '0x6B175474E89094C44Da98b954EedeAC495271d0F': results[1],    // DAI
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F': results[1],   // DAI
       '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': results[2],   // USDC
       '0xdAC17F958D2ee523a2206206994597C13D831ec7': results[3],   // USDT
-      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': results[4],   // WBTC
-      '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D': Math.trunc(results[5])  // RENBTC
-                                                      + Math.trunc(results[5]), //crvRENWBTC, estimate
+      '0x0000000000085d4780B73119b644AE5ecd22b376': results[4],   // TUSD
+      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': results[5],   // WBTC
+      '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D': Math.trunc(results[6])  // RENBTC
+                                                      + Math.trunc(results[6]), //crvRENWBTC, estimate
       // TODO Uniswap LP underlying
     };
 
