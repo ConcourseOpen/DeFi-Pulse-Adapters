c5db397326ebd4ec2e8ecd017fac90feea6b1650
mayowa <mtudonu85@gmail.com>
2020-11-04 21:18:58 +0100

    wrap bancor balance with toSymbols

--- projects/bancor/index.js
+++ projects/bancor/index.js
@@ -191,7 +191,7 @@
 
     sdk.util.sumMultiBalanceOf(balances, result);
 
-    return balances;
+    return (await sdk.api.util.toSymbols(balances)).output;
   }
 
 /*==================================================
