861fcc5379b5c0aa514c9cb47a3c6912f6897abb
Jude Dike <dikejude49@gmail.com>
2020-11-12 12:13:04 +0100

    remove running comments

--- projects/definer/index.js
+++ projects/definer/index.js
@@ -89,7 +89,7 @@ async function tvl(timestamp, block) {
     '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2': "0",
     '0x514910771AF9Ca656af840dff83E8264EcF986CA': "0"
   };
-  console.log({block: 10819469})
+  console.log({block, timestamp})
   if (block > 10819469) {
     // Get all Tokens in the market
     let markets = await utility.getMarkets(block);
@@ -102,9 +102,9 @@ async function tvl(timestamp, block) {
       }
     });
   }
-  // return balances
-  return (await sdk.api.util.toSymbols(balances)).output;;
+  return (await sdk.api.util.toSymbols(balances)).output;
 }
+
 /*==================================================
   Exports
   ==================================================*/
