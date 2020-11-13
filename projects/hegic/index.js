49951f89ade40a486866a75af86e159dcad73029
Chaz Schmidt <schmidt.864@osu.edu>
2020-11-13 16:23:45 -0500

    Update index.js

--- projects/hegic/index.js
+++ projects/hegic/index.js
@@ -40,7 +40,7 @@
   ==================================================*/
 
   module.exports = {
-    name: 'HEGIC',
+    name: 'Hegic',
     token: 'HEGIC',
     category: 'Derivatives',
     start: 1602360000, // Oct-10-2020 08:00:00 PM +UTC,
