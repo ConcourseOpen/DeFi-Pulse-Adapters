81e102d2c75d93cf2a5335ad3933a8060504c75d
Jeremy Yang <jeremy@cream.finance>
2020-11-27 11:13:32 +0800

    Update CREAM TVL

--- projects/cream/index.js
+++ projects/cream/index.js
@@ -16,10 +16,8 @@
   const markets = {};
   const marketsToIgnore = ['0xBdf447B39D152d6A234B4c02772B8ab5D1783F72'];
   const wETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-  const usdt = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-  const yCrv = '0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8';
-  const yyCrv = '0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c';
   const yETH = '0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7';
+  const CRETH2 = '0xcBc1065255cBc3aB41a6868c22d1f1C573AB89fd';
 
 /*==================================================
   TVL
@@ -79,18 +77,21 @@ async function tvl(timestamp, block) {
     abi: abi['getCash'],
   });
 
-  const yVaultPrices = (await sdk.api.abi.multiCall({
+  const yETHPirce = (await sdk.api.abi.call({
     block,
-    calls: [{target: yETH}, {target: yyCrv}],
-    abi: abi['getPricePerFullShare'],
+    target: yETH,
+    params: [],
+    abi: abi['getPricePerFullShare']
   })).output;
 
-  const yCrvPrice = (await sdk.api.abi.call({
-    block,
-    target: '0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51',
-    params: [],
-    abi: abi['get_virtual_price']
-  })).output;
+  const creth2Price =
+    block > 11282434 ?
+      (await sdk.api.abi.call({
+        block,
+        target: '0xbc338CA728a5D60Df7bc5e3AF5b6dF9DB697d942',
+        params: [wETH, CRETH2],
+        abi: abi['getSpotPrice']
+    })).output : 0;
 
 
   _.each(markets, (data, underlying) => {
@@ -98,23 +99,16 @@ async function tvl(timestamp, block) {
     if (getCash) {
       if (underlying === yETH) {
         const ethCash = BigNumber(balances[wETH] || 0);
-        const yETHCash = BigNumber(getCash.output).multipliedBy(yVaultPrices[0].output).div(1e18).integerValue();
+        const yETHCash = BigNumber(getCash.output).multipliedBy(yETHPirce).div(1e18).integerValue();
         balances[wETH] = ethCash.plus(yETHCash).toFixed();
-      } else if (underlying === yCrv) {
-        const usdtCash = BigNumber(balances[usdt] || 0);
-        const yCrvCash = BigNumber(getCash.output).multipliedBy(yCrvPrice).div(1e18).div(1e12).integerValue();
-        balances[usdt] = usdtCash.plus(yCrvCash).toFixed();
-      } else if (underlying === yyCrv) {
-        const usdtCash = BigNumber(balances[usdt] || 0);
-        const yyCrvCash = BigNumber(getCash.output).multipliedBy(yCrvPrice).div(1e18).div(1e12).multipliedBy(yVaultPrices[1].output).div(1e18).integerValue();
-        balances[usdt] = usdtCash.plus(yyCrvCash).toFixed();
+        delete balances[underlying];
+      } else if (underlying === CRETH2) {
+        const ethCash = BigNumber(balances[wETH] || 0);
+        const creth2Cash = BigNumber(getCash.output).multipliedBy(creth2Price).div(1e18).integerValue();
+        balances[wETH] = ethCash.plus(creth2Cash).toFixed();
+        delete balances[underlying];
       } else {
-        if (underlying === usdt) {
-          const usdtCash = BigNumber(balances[usdt] || 0);
-          balances[underlying] = usdtCash.plus(getCash.output).toFixed();
-        } else {
-          balances[underlying] = BigNumber(getCash.output).toFixed();
-        }
+        balances[underlying] = BigNumber(getCash.output).toFixed();
       }
     }
   });
