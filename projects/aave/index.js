d3dfeb6f7d212ebf692fa58d1e8e96744244a86b
David Truong <david@truong.vc>
2020-12-05 05:45:16 +0100

    [Aave] Update for v2 TVL

--- projects/aave/index.js
+++ projects/aave/index.js
@@ -22,6 +22,8 @@
   const aaveStakingContract = "0x4da27a545c0c5b758a6ba100e3a049001de870f5";
   const aaveTokenAddress = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
 
+  const addressesProviderRegistry = "0x52D306e36E3B6B02c153d0266ff0f85d18BCD413";
+
 /*==================================================
   Helper Functions
   ==================================================*/
@@ -36,7 +38,7 @@
     ).output;
   }
 
-  async function _getAssets(lendingPoolCore) {
+  async function _getV1Assets(lendingPoolCore) {
     const reserves = (
       await sdk.api.abi.call({
         target: lendingPoolCore,
@@ -84,7 +86,7 @@
     return assets
   }
 
-  async function _multiMarketTvl(lendingPoolCore, reserves, block) {
+  async function _multiMarketV1Tvl(lendingPoolCore, reserves, block) {
     let balances = {
       "0x0000000000000000000000000000000000000000": (
         await sdk.api.eth.getBalance({ target: lendingPoolCore, block })
@@ -105,7 +107,7 @@
     return balances;
   }
 
-  async function _multiMarketRates(lendingPool, reserves, block) {
+  async function _multiMarketV1Rates(lendingPool, reserves, block) {
     const calls = _.map(reserves, (reserve) => ({
       target: lendingPool,
       params: reserve.address,
@@ -193,15 +195,109 @@
     return ratesData;
   }
 
-  async function getReserves() {
+  async function getV1Reserves() {
     if (aaveReserves.length === 0) {
-      aaveReserves = await _getAssets(aaveLendingPoolCore);
+      aaveReserves = await _getV1Assets(aaveLendingPoolCore);
     }
 
     if (uniswapReserves.length === 0) {
       // Does not take into account Uniswap LP assets (not yet supported on DeFiPulse)
-      uniswapReserves = await _getAssets(uniswapLendingPoolCore);
+      uniswapReserves = await _getV1Assets(uniswapLendingPoolCore);
     }
+  }
+
+  async function getV2Data() {
+    const addressesProviders = (
+      await sdk.api.abi.call({
+        target: addressesProviderRegistry,
+        abi: abi["getAddressesProvidersList"],
+      })
+    ).output;
+
+    const protocolDataHelpers = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(addressesProviders, (provider) => ({
+          target: provider,
+          params: "0x1",
+        })),
+        abi: abi["getAddress"],
+      })
+    ).output;
+
+    const aTokenMarketData = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(protocolDataHelpers, (dataHelper) => ({
+          target: dataHelper.output,
+        })),
+        abi: abi["getAllATokens"],
+      })
+    ).output;
+
+    let aTokenAddresses = []
+    aTokenMarketData.map(aTokensData => {
+      aTokenAddresses = [...aTokenAddresses, ...aTokensData.output.map(aToken => aToken[1])]
+    })
+
+    const underlyingAddresses = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(aTokenAddresses, (aToken) => ({
+          target: aToken
+        })),
+        abi: abi["getUnderlying"]
+      })
+    ).output
+
+    const decimalsOfUnderlying = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(underlyingAddresses, (underlying) => ({
+          target: underlying.output,
+        })),
+        abi: "erc20:decimals",
+      })
+    ).output;
+
+    const symbolsOfUnderlying = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(underlyingAddresses, (underlying) => ({
+          target: underlying.output,
+        })),
+        abi: "erc20:symbol",
+      })
+    ).output;
+
+    const underlyingAddressesDict = Object.keys(underlyingAddresses).map(
+      (key) => underlyingAddresses[key].output
+    );
+
+    const balanceOfUnderlying = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(aTokenAddresses, (aToken, index) => ({
+          target: underlyingAddressesDict[index],
+          params: aToken,
+        })),
+        abi: "erc20:balanceOf",
+      })
+    ).output;
+
+    const v2Data = balanceOfUnderlying.map((underlying, index) => {
+      const address = underlying.input.target
+      let symbol;
+      if (address == '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2') {
+        symbol = 'MKR'
+      } else {
+        symbol = symbolsOfUnderlying[index].output
+      }
+
+      return {
+        aToken: aTokenAddresses[index],
+        underlying: address,
+        symbol,
+        decimals: decimalsOfUnderlying[index].output,
+        balance: underlying.output
+      }
+    })
+
+    return v2Data
   }
 
 /*==================================================
@@ -209,16 +305,17 @@
   ==================================================*/
 
   async function tvl(timestamp, block) {
-    await getReserves()
-    let balances = await _multiMarketTvl(aaveLendingPoolCore, aaveReserves, block);
-    
-    const uniswapMarketTvlBalances = await _multiMarketTvl(
+    // V1 TVLs
+    await getV1Reserves()
+    let balances = await _multiMarketV1Tvl(aaveLendingPoolCore, aaveReserves, block);
+
+    const uniswapMarketTvlBalances = await _multiMarketV1Tvl(
       uniswapLendingPoolCore,
       uniswapReserves,
       block
     );
-
-    // Combine TVL values into one dict
+    
+    // ...add v1 uniswap market TVL
     Object.keys(uniswapMarketTvlBalances).forEach(address => {
       if (balances[address]) {
         balances[address] = BigNumber(
@@ -229,8 +326,24 @@
       }
     });
 
+    // Staking TVL
     const stakedAaveAmount = await _stakingTvl(block);
-    balances[aaveTokenAddress] = stakedAaveAmount;
+    balances[aaveTokenAddress] = balances[aaveTokenAddress] ?
+      BigNumber(balances[aaveTokenAddress]).plus(stakedAaveAmount).toFixed() :
+      stakedAaveAmount
+
+    // V2 TVLs
+    const v2Data = await getV2Data();
+    v2Data.map(data => {
+      if (balances[data.underlying]) {
+        balances[data.underlying] = BigNumber(balances[data.underlying])
+          .plus(data.balance)
+          .toFixed();
+      } else {
+        balances[data.underlying] = data.balance;
+      }
+    })
+
     return balances;
   }
 
@@ -239,7 +352,7 @@
   ==================================================*/
 
   async function rates(timestamp, block) {
-    await getReserves()
+    await getV1Reserves()
   
     // DeFi Pulse only supports single market atm, so no rates from Uniswap market (e.g. Dai on Uniswap market)
     const aaveReservesWithEth = aaveReserves
@@ -248,7 +361,7 @@
       symbol: "ETH",
       decimals: 18,
     });
-    return await _multiMarketRates(aaveLendingPool, aaveReserves, block)
+    return await _multiMarketV1Rates(aaveLendingPool, aaveReserves, block)
   }
 
 /*==================================================
@@ -258,7 +371,7 @@
   module.exports = {
     name: "Aave",
     website: "https://aave.com",
-    token: "LEND",
+    token: "AAVE",
     category: "lending",
     start: 1578355200, // 01/07/2020 @ 12:00am (UTC)
     tvl,
