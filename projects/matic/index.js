39869ef14f9a835c1da92542def7e5b254fa0e5d
nedodn <odennate@gmail.com>
2020-12-28 10:48:24 -0600

    remove staked matic

--- projects/matic/index.js
+++ projects/matic/index.js
@@ -16,10 +16,10 @@ async function tvl(timestamp, block) {
     }
 
     const posTokens = [
-        {
-            target: maticToken,
-            params: stakeManager
-        },
+        // {
+        //     target: maticToken,
+        //     params: stakeManager
+        // },
         {
             target: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
             params: posERC20Predicate
