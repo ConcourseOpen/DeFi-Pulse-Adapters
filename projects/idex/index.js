aeb09e9665b86dc9617cb123a065dc29a39ff51f
Braden Napier <bradynapier@gmail.com>
2021-01-17 23:19:10 -0500

    feat(adapter/idex): add initial idex adapter template

---
+++ projects/idex/index.js
@@ -0,0 +1,32 @@
+/*==================================================
+  Modules
+  ==================================================*/
+
+  const sdk = require('../../sdk');
+
+const IDEX_CUSTODY_CONTRACT = '0xE5c405C5578d84c5231D3a9a29Ef4374423fA0c2';
+
+/*==================================================
+  TVL
+  ==================================================*/
+
+  async function tvl(timestamp, block) {
+    let balances = {
+      '0x0000000000000000000000000000000000000000': 1000000000000000000, // ETH
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F': 2000000000000000000  // DAI
+    };
+
+    return balances;
+  }
+
+/*==================================================
+  Exports
+  ==================================================*/
+
+  module.exports = {
+    name: 'Template Project', // project name
+    token: null,              // null, or token symbol if project has a custom token
+    category: 'assets',       // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
+    start: 1514764800,        // unix timestamp (utc 0) specifying when the project began, or where live data begins
+    tvl                       // tvl adapter
+  }
