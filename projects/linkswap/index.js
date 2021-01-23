27726933a3634a6845ce6d9c942e7cea7d80aecc
Giovanni Di Siena <giodisiena@gmail.com>
2021-01-23 19:15:58 +0000

    Remove governance staking TVL
    DeFi Pulse do not include this in calculation of  TVL

--- projects/linkswap/index.js
+++ projects/linkswap/index.js
@@ -4,18 +4,6 @@ const v1TVL = require('./v1');
 
 const ETH = '0x0000000000000000000000000000000000000000';
 const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-const YFL = '0x28cb7e841ee97947a86B06fA4090C8451f64c0be';
-const yYFL = '0x75d1aa733920b14fc74c9f6e6fab7ac1ece8482e';
-
-async function _stakingTvl(block) {
-  return (
-    await sdk.api.abi.call({
-      target: YFL,
-      params: yYFL,
-      abi: "erc20:balanceOf",
-    })
-  ).output;
-}
 
 async function tvl(timestamp, block) {
   const [v1] = await Promise.all([v1TVL(timestamp, block)]);
@@ -36,12 +24,6 @@ async function tvl(timestamp, block) {
         return accumulator
       }, {})
   );
-  
-  // Governance Staking TVL
-  const stakedYFLAmount = await _stakingTvl(block);
-  balances[YFL] = balances[YFL] ?
-    BigNumber(balances[YFL]).plus(stakedYFLAmount).toFixed() :
-    stakedYFLAmount
 
   return balances;
 }
