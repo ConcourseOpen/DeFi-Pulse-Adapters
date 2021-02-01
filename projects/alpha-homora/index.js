5a6b221bfdad59b2bc4154e0316375a354726ab3
nedodn <odennate@gmail.com>
2021-02-01 15:36:17 -0600

    totalCollateral check

--- projects/alpha-homora/index.js
+++ projects/alpha-homora/index.js
@@ -189,7 +189,7 @@ async function tvlV2(timestamp, block) {
         : 0
     )
   );
-  return totalCollateral;
+  return totalCollateral.isNaN() ? Bignumber(0) : totalCollateral;
 }
 
 async function getLpTokenPrices(lpTokens, block) {
