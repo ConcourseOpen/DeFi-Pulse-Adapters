f76c67172ddaf3e87af1ddf27e34a55c5ae55fd4
Cussone <aleksandar.cakalic@gmail.com>
2021-02-24 09:41:06 +0100

    Add condition to prevent calling non-existing contracts in defisaver

--- projects/defisaver/index.js
+++ projects/defisaver/index.js
@@ -2,24 +2,30 @@ require('dotenv').config();
 
 const sdk = require('../../sdk');
 
-const { getContractAddress, getContractMethod  } = require('./utils');
+const { getContractAddress, getContractMethod, getContractBlock  } = require('./utils');
 
 async function tvl(timestamp, block) {
-  const { output: makerSubs } = (await sdk.api.abi.call({
+  let makerSubs = [];
+  let compoundSubs = [];
+  let aaveSubs = [];
+
+  if (block >= getContractBlock('McdSubscriptions')) makerSubs = (await sdk.api.abi.call({
     block,
     target: getContractAddress('McdSubscriptions'),
     abi: getContractMethod('getSubscribers', 'McdSubscriptions'),
-  }));
-  const { output: compoundSubs } = (await sdk.api.abi.call({
+  })).output;
+
+  if (block >= getContractBlock('CompoundSubscriptions')) compoundSubs = (await sdk.api.abi.call({
     block,
     target: getContractAddress('CompoundSubscriptions'),
     abi: getContractMethod('getSubscribers', 'CompoundSubscriptions'),
-  }));
-  const { output: aaveSubs } = (await sdk.api.abi.call({
+  })).output;
+
+  if (block >= getContractBlock('AaveSubscriptions')) aaveSubs = (await sdk.api.abi.call({
     block,
     target: getContractAddress('AaveSubscriptions'),
     abi: getContractMethod('getSubscribers', 'AaveSubscriptions'),
-  }));
+  })).output;
 
   const subscribers = new Set();
 
@@ -27,12 +33,10 @@ async function tvl(timestamp, block) {
   for (const sub of compoundSubs) { subscribers.add(sub[0]); }
   for (const sub of aaveSubs) { subscribers.add(sub[0]); }
 
-  const { output: assetsLocked } = await sdk.api.cdp.getAssetsLocked({
+  return (await sdk.api.cdp.getAssetsLocked({
     block,
     targets: [...subscribers]
-  });
-
-  return assetsLocked;
+  })).output;
 }
 
 module.exports = {
@@ -41,6 +45,6 @@ module.exports = {
   token: null,
   website: 'https://defisaver.com/',
   category: 'lending',
-  start: 1588723200, // 06/05/2020 @ 02:00 (UTC)
+  start: 1586430640, // 09/04/2020 @ 11:10 (UTC)
   contributesTo: ['Maker', 'Compound', 'Aave'],
 };
