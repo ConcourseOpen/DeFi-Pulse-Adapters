6f4e72955e1b7ccea24d786291dbb09f2fa1695c
David Truong <david@truong.vc>
2021-03-02 16:33:45 +0100

    merge

--- projects/aave/index.js
+++ projects/aave/index.js
@@ -25,6 +25,9 @@
   const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
   const addressesProviderRegistry = "0x52D306e36E3B6B02c153d0266ff0f85d18BCD413";
+  let v2Atokens = [];
+  let v2ReserveTokens = [];
+  let addressSymbolMapping = {};
 
 /*==================================================
   Helper Functions
@@ -164,7 +167,7 @@
       const reserveDict = reserves.find(
         (reserve) => reserve.address === address
       );
-      const symbol = reserveDict.symbol;
+      const symbol = reserveDict.symbol + "_V1";
 
       const outStandingBorrows = BigNumber(reserveData.totalLiquidity)
         .minus(BigNumber(reserveData.availableLiquidity))
@@ -193,7 +196,7 @@
       const reserveDict = reserves.find(
         (reserve) => reserve.address === address
       );
-      const symbol = reserveDict.symbol;
+      const symbol = reserveDict.symbol + "_V1";
 
       const isActive = reserveConfig.isActive;
       const borrowingEnabled = reserveConfig.borrowingEnabled;
@@ -232,108 +235,189 @@
     }
   }
 
-  async function getV2Data(block) {
-    try {
-      const addressesProviders = (
-        await sdk.api.abi.call({
-          target: addressesProviderRegistry,
-          abi: abi["getAddressesProvidersList"],
-          block
-        })
-      ).output;
+  async function getV2Reserves() {
+    if (v2Atokens.length !== 0 && v2ReserveTokens.length !== 0) return
+  
+    const addressesProviders = (
+      await sdk.api.abi.call({
+        target: addressesProviderRegistry,
+        abi: abi["getAddressesProvidersList"],
+      })
+    ).output;
 
-      const protocolDataHelpers = (
-        await sdk.api.abi.multiCall({
-          calls: _.map(addressesProviders, (provider) => ({
-            target: provider,
-            params: "0x1",
-          })),
-          abi: abi["getAddress"],
-          block
-        })
-      ).output;
-
-      const aTokenMarketData = (
-        await sdk.api.abi.multiCall({
-          calls: _.map(protocolDataHelpers, (dataHelper) => ({
-            target: dataHelper.output,
-          })),
-          abi: abi["getAllATokens"],
-          block
-        })
-      ).output;
-
-      let aTokenAddresses = []
-      aTokenMarketData.map(aTokensData => {
-        if(aTokensData.output && aTokensData.output.length)
-          aTokenAddresses = [...aTokenAddresses, ...aTokensData.output.map(aToken => aToken[1])]
+    const protocolDataHelpers = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(addressesProviders, (provider) => ({
+          target: provider,
+          params: "0x1",
+        })),
+        abi: abi["getAddress"],
       })
 
-      const underlyingAddresses = (
-        await sdk.api.abi.multiCall({
-          calls: _.map(aTokenAddresses, (aToken) => ({
-            target: aToken
-          })),
-          abi: abi["getUnderlying"],
-          block
-        })
-      ).output
-
-      const decimalsOfUnderlying = (
-        await sdk.api.abi.multiCall({
-          calls: _.map(underlyingAddresses, (underlying) => ({
-            target: underlying.output,
-          })),
-          abi: "erc20:decimals",
-          block
-        })
-      ).output;
-
-      const symbolsOfUnderlying = (
-        await sdk.api.abi.multiCall({
-          calls: _.map(underlyingAddresses, (underlying) => ({
-            target: underlying.output,
-          })),
-          abi: "erc20:symbol",
-          block
-        })
-      ).output;
-
-      const underlyingAddressesDict = Object.keys(underlyingAddresses).map(
-        (key) => underlyingAddresses[key].output
-      );
-
-      const balanceOfUnderlying = (
-        await sdk.api.abi.multiCall({
-          calls: _.map(aTokenAddresses, (aToken, index) => ({
-            target: underlyingAddressesDict[index],
-            params: aToken,
-          })),
-          abi: "erc20:balanceOf",
-          block
-        })
-      ).output;
-
-      const v2Data = balanceOfUnderlying.map((underlying, index) => {
-        const address = underlying.input.target
-        let symbol;
-        if (address == '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2') {
-          symbol = 'MKR'
-        } else {
-          symbol = symbolsOfUnderlying[index].output
-        }
-
-        return {
-          aToken: aTokenAddresses[index],
-          underlying: address,
-          symbol,
-          decimals: decimalsOfUnderlying[index].output,
-          balance: underlying.output
-        }
+    const aTokenMarketData = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(protocolDataHelpers, (dataHelper) => ({
+          target: dataHelper.output,
+        })),
+        abi: abi["getAllATokens"],
       })
+    ).output;
+
+    let aTokenAddresses = [];
+    aTokenMarketData.map((aTokensData) => {
+      aTokenAddresses = [
+        ...aTokenAddresses,
+        ...aTokensData.output.map((aToken) => aToken[1]),
+      ];
+    });
+
+    const underlyingAddressesData = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(aTokenAddresses, (aToken) => ({
+          target: aToken,
+        })),
+        abi: abi["getUnderlying"],
+      })
+    ).output;
+
+    let reserveAddresses = [];
+    underlyingAddressesData.map((reserveData) => {
+      reserveAddresses.push(reserveData.output)
+    });
+
+    v2Atokens = aTokenAddresses
+    v2ReserveTokens = reserveAddresses;
+
+    // Fetch associated token info
+    const symbolsOfReserves = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(v2ReserveTokens, (underlying) => ({
+          target: underlying,
+        })),
+        abi: "erc20:symbol",
+      })
+    ).output;
+
+    const decimalsOfReserves = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(v2ReserveTokens, (underlying) => ({
+          target: underlying,
+        })),
+        abi: "erc20:decimals",
+      })
+    ).output;
+
+    symbolsOfReserves.map((r) => {
+      const address = r.input.target;
+      let symbol;
+
+      if (address == "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2") {
+        symbol = "MKR";
+      } else {
+        symbol = r.output;
+      }
+
+      addressSymbolMapping[address] = { symbol };
+    });
+
+    decimalsOfReserves.map((r) => {
+      const address = r.input.target;
+      const existingAddress = addressSymbolMapping[address];
+      addressSymbolMapping[address] = {
+        ...existingAddress,
+        decimals: r.output,
+      };
+    });
+  }
+
+  async function getV2Tvl() {
+    const underlyingAddressesDict = Object.keys(v2ReserveTokens).map(
+      (key) => v2ReserveTokens[key]
+    );
+
+    const balanceOfUnderlying = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(v2Atokens, (aToken, index) => ({
+          target: underlyingAddressesDict[index],
+          params: aToken,
+        })),
+        abi: "erc20:balanceOf",
+      })
+    ).output;
+
+    const v2Data = balanceOfUnderlying.map((underlying, index) => {
+      const address = underlying.input.target
+      return {
+        aToken: v2Atokens[index],
+        underlying: address,
+        symbol: addressSymbolMapping[address].symbol,
+        decimals: addressSymbolMapping[address].decimals,
+        balance: underlying.output,
+      };
+    })
 
       return v2Data
     }catch (e) {console.log(e.message)}
+  }
+
+  async function getV2Rates() {
+    const addressesProviders = (
+      await sdk.api.abi.call({
+        target: addressesProviderRegistry,
+        abi: abi["getAddressesProvidersList"],
+      })
+    ).output;
+
+    const protocolDataHelpers = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(addressesProviders, (provider) => ({
+          target: provider,
+          params: "0x1",
+        })),
+        abi: abi["getAddress"],
+      })
+    ).output;
+
+    const reserveData = (
+      await sdk.api.abi.multiCall({
+        calls: _.map(v2ReserveTokens, (reserve) => ({
+          target: protocolDataHelpers[0].output,
+          params: reserve,
+        })),
+        abi: abi["getReserveDataV2"],
+      })
+    ).output
+
+    let ratesData = { lend: {}, borrow: {}, supply: {}, borrow_stable: {} };
+    
+    reserveData.map(result => {
+      if (!result || !result.success) return;
+      const address = result.input.params[0]
+      const reserveDetails = addressSymbolMapping[address]
+      const symbol = reserveDetails.symbol + "_V2";
+      const details = result.output;
+
+      const outStandingBorrows = BigNumber(details.totalStableDebt)
+        .plus(BigNumber(details.totalVariableDebt))
+        .div(10 ** reserveDetails.decimals)
+        .toFixed();
+      const lendRate = BigNumber(details.liquidityRate)
+        .div(10 ** 25)
+        .toFixed();
+      const borrowRate = BigNumber(details.variableBorrowRate)
+        .div(10 ** 25)
+        .toFixed();
+      const borrowStableRate = BigNumber(details.stableBorrowRate)
+        .div(10 ** 25)
+        .toFixed();
+
+      ratesData.lend[symbol] = lendRate;
+      ratesData.borrow[symbol] = borrowRate;
+      ratesData.borrow_stable[symbol] = borrowStableRate;
+      ratesData.supply[symbol] = outStandingBorrows;
+    })
+
+    return ratesData
   }
 
 /*==================================================
@@ -378,20 +462,17 @@
     })
 
     // V2 TVLs
-    if (block >= 11360925) {
-      const v2Data = await getV2Data(block);
-      if(v2Data) {
-        v2Data.map(data => {
-          if (balances[data.underlying]) {
-            balances[data.underlying] = BigNumber(balances[data.underlying])
-              .plus(data.balance)
-              .toFixed();
-          } else {
-            balances[data.underlying] = data.balance;
-          }
-        })
+    await getV2Reserves()
+    const v2Tvl = await getV2Tvl();
+    v2Tvl.map(data => {
+      if (balances[data.underlying]) {
+        balances[data.underlying] = BigNumber(balances[data.underlying])
+          .plus(data.balance)
+          .toFixed();
+      } else {
+        balances[data.underlying] = data.balance;
       }
-    }
+    })
 
     return (await sdk.api.util.toSymbols(balances)).output;
   }
@@ -402,15 +483,25 @@
 
   async function rates(timestamp, block) {
     await getV1Reserves()
-
-    // DeFi Pulse only supports single market atm, so no rates from Uniswap market (e.g. Dai on Uniswap market)
+  
     const aaveReservesWithEth = aaveReserves
     aaveReservesWithEth.push({
       address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
       symbol: "ETH",
       decimals: 18,
     });
-    return await _multiMarketV1Rates(aaveLendingPool, aaveReserves, block)
+
+    const ratesV1 = await _multiMarketV1Rates(aaveLendingPool, aaveReserves, block)
+
+    await getV2Reserves()
+    const ratesV2 = await getV2Rates()
+
+    return { 
+      lend: { ...ratesV1.lend, ...ratesV2.lend }, 
+      borrow: { ...ratesV1.borrow, ...ratesV2.borrow }, 
+      borrow_stable: { ...ratesV1.borrow_stable, ...ratesV2.borrow_stable },
+      supply: { ...ratesV1.supply, ...ratesV2.supply }, 
+    };
   }
 
 /*==================================================
