617e9bdd3da6d5a939050f9dcaf3047670072553
seafi <74265744+seafi@users.noreply.github.com>
2021-03-01 17:48:55 -0800

    Siren: get pools dynamically

--- projects/siren/index.js
+++ projects/siren/index.js
@@ -1,41 +1,55 @@
 /*==================================================
   Modules
   ==================================================*/
-
+  const { request, gql } = require("graphql-request");
+  const BigNumber = require('bignumber.js');
   const sdk = require('../../sdk');
-  const pools = {
-    USDC: '0x25bc339170AdBfF2B7b9edE682072577Fa9d96E8',
-    WBTC: '0x87a3eF113C210Ab35AFebe820fF9880bf0DD4bfC',
-  }
-  const assets = {
-    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    WBTC: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
-  }
+
+  const GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/sirenmarkets/protocol';
 
 /*==================================================
   TVL
   ==================================================*/
 
   async function tvl(timestamp, block) {
-    const WBTCRequest = sdk.api.erc20.balanceOf({
+    const {
+      amms,
+    } = await request(GRAPH_URL, GET_POOLS, {
       block,
-      target: assets.WBTC,
-      owner: pools.WBTC,
-    })
+    });
 
-    const USDCRequest = sdk.api.erc20.balanceOf({
-      block,
-      target: assets.USDC,
-      owner: pools.USDC,
-    })
-
-    const [USDCResponse, WBTCResponse] = await Promise.all([USDCRequest, WBTCRequest])
-
-    return {
-      [assets.USDC]: USDCResponse.output,
-      [assets.WBTC]: WBTCResponse.output,
+    const result = {
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '0'
     };
+
+    for (let i = 0; i < amms.length; i++) {
+      const amm = amms[i];
+      const collateralToken = amm['collateralToken']['id'];
+      const response = await sdk.api.erc20.balanceOf({
+        block,
+        target: collateralToken,
+        owner: amm['id'],
+      });
+
+      if (!result[collateralToken]) {
+        result[collateralToken] = '0';
+      }
+      result[collateralToken] = BigNumber(result[collateralToken]).plus(response.output).toFixed();
+    }
+
+    return result;
   }
+
+  const GET_POOLS = gql`
+    query Pools($block: Int) {
+      amms(block: { number: $block }) {
+        id
+        collateralToken {
+          id
+  }
+      }
+    }
+  `
 
 /*==================================================
   Exports
