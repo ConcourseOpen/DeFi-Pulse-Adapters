e863cb59e2e30c3b9a4e514e2dd1ab15cc4f768b
arman37 <arman.it37@gmail.com>
2021-03-11 19:32:16 +0600

    Translate tornado into v2

---
+++ v2/projects/tornado/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /* Project Metadata */
+  name: "Tornado Cash",
+  token: "TORN",
+  category: "Payments",
+  start: 1576497600,
+  /* required for indexing token balances */
+  tokenHolderMap: [
+    {
+      tokens: '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490',
+      holders: [
+        "0x12d66f87a04a9e220743712ce6d9bb1b5616b8fc", // 0.1 ETH
+        "0x47ce0c6ed5b0ce3d3a51fdb1c52dc66a7c3c2936", // 1 ETH
+        "0x910cbd523d972eb0a6f4cae4618ad62622b39dbf", // 10 ETH
+        "0xa160cdab225685da1d56aa342ad8841c3b53f291", // 100 ETH
+      ],
+      checkETHBalance: true,
+    }
+  ]
+};
