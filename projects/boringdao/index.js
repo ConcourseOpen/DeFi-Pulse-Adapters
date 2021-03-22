b5742f8b4184044d964728548434e285b856c6d9
enki.y <enki.y@boringdao.com>
2021-03-22 23:07:24 +0800

    Add BoringDAO Adapter

--- projects/boringdao/index.js
+++ projects/boringdao/index.js
@@ -8,11 +8,7 @@ const sdk = require('../../../sdk');
   Settings
 ==================================================*/
 
-const BORContract = '0x3c9d6c1c73b31c837832c72e04d3152f051fc1a9';
 const oBTCContract = '0x8064d9ae6cdf087b1bcd5bdf3531bd5d8c537a68';
-const oLTCContract = '0x07C44B5Ac257C2255AA0933112c3b75A6BFf3Cb1';
-
-
 
 /*==================================================
   Main
@@ -36,9 +32,9 @@ async function balance(timestamp, block) {
 ==================================================*/
 
 module.exports = {
-    name: 'BoringDAO BTC',
+    name: 'BoringDAO oBTC',
     symbol: "oBTC",
-    type: 'Hybrid',
+    type: 'hybrid',
     start: 1605269554, // Nov-12-2020 08:25:54 AM +UTC
     balance,
 };
