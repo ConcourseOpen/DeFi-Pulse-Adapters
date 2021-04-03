f65392ec4367ab99263f9d4518b2317445bf1c49
nedodn <odennate@gmail.com>
2021-04-03 13:41:29 -0500

    compound fix

--- projects/compound/index.js
+++ projects/compound/index.js
@@ -12,58 +12,68 @@ const BigNumber = require('bignumber.js');
   ==================================================*/
 
   // cache some data
-  const markets = {
-    '0x0D8775F648430679A709E98d2b0Cb6250d2887EF': {
+  const markets = [
+    {
+      underlying: '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
       symbol: 'BAT',
       decimals: 18,
       cToken: '0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E',
     },
-    '0x6B175474E89094C44Da98b954EedeAC495271d0F': {
+    {
+      underlying: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
       symbol: 'DAI',
       decimals: 18,
       cToken: '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
     },
-    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': {
+    {
+      underlying: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
       symbol: 'WETH',
       decimals: 18,
       cToken: '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5',
     }, //cETH => WETH
-    '0x1985365e9f78359a9B6AD760e32412f4a445E862': {
+    {
+      underlying: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
       symbol: 'REP',
       decimals: 18,
       cToken: '0x158079Ee67Fce2f58472A96584A73C7Ab9AC95c1',
     },
-    '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': {
+    {
+      underlying: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
       symbol: 'USDC',
       decimals: 6,
       cToken: '0x39AA39c021dfbaE8faC545936693aC917d5E7563',
     },
-    '0xdAC17F958D2ee523a2206206994597C13D831ec7': {
+    {
+      underlying: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
       symbol: 'USDT',
       decimals: 6,
       cToken: '0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9',
     },
-    '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': {
+    {
+      underlying: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
       symbol: 'WBTC',
       decimals: 8,
       cToken: '0xC11b1268C1A384e55C48c2391d8d480264A3A7F4',//cWBTC - legacy
     },
-    '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': {
+    {
+      underlying: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
       symbol: 'WBTC',
       decimals: 8,
       cToken: '0xccf4429db6322d5c611ee964527d42e5d685dd6a'//cWBTC
     },
-    '0xE41d2489571d322189246DaFA5ebDe1F4699F498': {
+    {
+      underlying: '0xE41d2489571d322189246DaFA5ebDe1F4699F498',
       symbol: 'ZRX',
       decimals: 18,
       cToken: '0xB3319f5D18Bc0D84dD1b4825Dcde5d5f7266d407',
     },
-    '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359': {
+    {
+      underlying: '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359',
       symbol: 'SAI',
       decimals: 18,
       cToken: '0xF5DCe57282A584D2746FaF1593d3121Fcac444dC',
     },
-  };
+  ];
 
 /*==================================================
   TVL
@@ -101,11 +111,16 @@ async function getMarkets(block) {
     // if not in cache, get from the blockchain
     await (
       Promise.all(allCTokens.map(async (cToken) => {
-        let underlying = await getUnderlying(block, cToken);
-
-        if (!markets[underlying]) {
+        let foundMarket = false;
+        for (let market of markets) {
+          if (market.cToken.toLowerCase() === cToken.toLowerCase()) {
+            foundMarket = true;
+          }
+        }
+        if (!foundMarket) {
+          let underlying = await getUnderlying(block, cToken);
           let info = await sdk.api.erc20.info(underlying);
-          markets[underlying] = { cToken, decimals: info.output.decimals, symbol: info.output.symbol };
+          markets.push({ underlying, cToken, decimals: info.output.decimals, symbol: info.output.symbol })
         }
       }))
     );
@@ -121,8 +136,8 @@ async function tvl(timestamp, block) {
   // Get V1 tokens locked
   let v1Locked = await sdk.api.abi.multiCall({
     block,
-    calls: _.map(markets, (data, underlying) => ({
-      target: underlying,
+    calls: _.map(markets, (market) => ({
+      target: market.underlying,
       params: '0x3FDA67f7583380E67ef93072294a7fAc882FD7E7',
     })),
     abi: 'erc20:balanceOf',
@@ -133,17 +148,17 @@ async function tvl(timestamp, block) {
   // Get V2 tokens locked
   let v2Locked = await sdk.api.abi.multiCall({
     block,
-    calls: _.map(markets, (data, underlying) => ({
-      target: data.cToken,
+    calls: _.map(markets, (market) => ({
+      target: market.cToken,
     })),
     abi: abi['getCash'],
   });
 
-  _.each(markets, (data, underlying) => {
-    let getCash = _.find(v2Locked.output, (result) => result.success && result.input.target === data.cToken);
+  _.each(markets, (market) => {
+    let getCash = _.find(v2Locked.output, (result) => result.success && result.input.target === market.cToken);
 
     if (getCash) {
-      balances[underlying] = BigNumber(balances[underlying] || 0)
+      balances[market.underlying] = BigNumber(balances[market.underlying] || 0)
         .plus(getCash.output)
         .toFixed();
     }
@@ -167,7 +182,7 @@ async function rates(timestamp, block) {
   // V2
   const markets = await getMarkets();
 
-  const calls = _.map(markets, (data, underlying) => ({
+  const calls = _.map(markets, (data) => ({
     target: data.cToken,
   }));
 
@@ -189,7 +204,7 @@ async function rates(timestamp, block) {
     abi: abi['totalBorrows'],
   })).output;
 
-  _.each(markets, (data, underlying) => {
+  _.each(markets, (data) => {
     let supplyRate = _.find(
       supplyResults,
       (result) => result.success && result.input.target == data.cToken
@@ -215,7 +230,7 @@ async function rates(timestamp, block) {
         .div(10 ** data.decimals)
         .toFixed();
     } else {
-      v1Tokens[underlying] = data;
+      v1Tokens[data.underlying] = data;
     }
   });
 
@@ -225,9 +240,9 @@ async function rates(timestamp, block) {
 
     const marketsResults = (await sdk.api.abi.multiCall({
       block,
-      calls: _.map(v1Tokens, (data, underlying) => ({
+      calls: _.map(v1Tokens, (data) => ({
         target: '0x3FDA67f7583380E67ef93072294a7fAc882FD7E7',
-        params: underlying,
+        params: data.underlying,
       })),
       abi: abi['markets'],
     })).output;
