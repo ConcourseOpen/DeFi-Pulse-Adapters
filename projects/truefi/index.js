342fda278b36d15025cffb87438ff6ea04ef1af2
mayowa <mtudonu85@gmail.com>
2021-04-04 09:08:52 +0100

    update trufi adapter

--- projects/truefi/index.js
+++ projects/truefi/index.js
@@ -1,27 +1,31 @@
 const sdk = require('../../sdk');
-const abi = require('./abi');
+const abi = require('./abi.json');
 
 
 const POOL = '0xa1e72267084192db7387c8cc1328fade470e4149';
-const RATING = '0x43A4F930F2cC35948d3a6dcd47CD0E50761f9B88';
+const stkTRU = '0x23696914Ca9737466D8553a2d619948f548Ee424';
 const TRU = '0x4C19596f5aAfF459fA38B0f7eD92F11AE6543784';
 const TUSD = '0x0000000000085d4780B73119b644AE5ecd22b376';
 
 
 async function tvl(timestamp, block) {
     let balances = {};
-
-    const poolTVL = await sdk.api.abi.call({
-      target: POOL,
-      abi: abi['poolValue'],
-      block: block
-    });
-    const truTVL = await sdk.api.abi.call({
-      target: TRU,
-      params: RATING,
-      abi: 'erc20:balanceOf',
-      block: block
-    });
+    let poolTVL;
+    let truTVL;
+    try {
+       poolTVL = await sdk.api.abi.call({
+        target: POOL,
+        abi: abi['poolValue'],
+        block: block
+      });
+    }catch (e){poolTVL = {output: '0'}}
+    try{
+      truTVL = await sdk.api.abi.call({
+        target: stkTRU,
+        abi: abi['stakeSupply'],
+        block: block
+      });
+    }catch (e) {truTVL = {output: '0'}}
 
     balances[TUSD] = poolTVL.output;
     balances[TRU] = truTVL.output;
