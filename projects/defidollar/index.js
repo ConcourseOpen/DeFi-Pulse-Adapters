52d4c67c150767ac9a12164c01e43b07e534a724
mayowa <mtudonu85@gmail.com>
2021-04-07 15:39:09 +0100

    update defi dollar adapter

--- projects/defidollar/index.js
+++ projects/defidollar/index.js
@@ -31,13 +31,28 @@ async function tvl(timestamp, block) {
     })
   });
 
-  let balanceOfResults = await sdk.api.abi.multiCall({
-    block,
-    calls,
-    abi: 'erc20:balanceOf'
-  });
+  try {
+    let balanceOfResults = await sdk.api.abi.multiCall({
+      block,
+      calls,
+      abi: 'erc20:balanceOf'
+    });
 
-  sdk.util.sumMultiBalanceOf(balances, balanceOfResults);
+    sdk.util.sumMultiBalanceOf(balances, balanceOfResults);
+  }catch (error){
+    balances = {
+      "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8": 0,
+      "0x5dbcf33d8c2e976c6b560249878e6f1491bca25c": 0
+    }
+  }
+
+  if(_.isEmpty(balances)){
+    balances = {
+      "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8": 0,
+      "0x5dbcf33d8c2e976c6b560249878e6f1491bca25c": 0
+    }
+  }
+
   return balances;
 }
 
