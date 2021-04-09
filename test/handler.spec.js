9f820f0afc568800ecd8ddcc83aaa9037b5d9dc2
yueqiaocertik <yue.qiao@certik.org>
2021-04-08 22:48:27 -0700

    fix test naming

--- test/handler.spec.js
+++ test/handler.spec.js
@@ -1,7 +1,7 @@
 const { handler } = require("../handler/handler");
 
 describe("handler", () => {
-  test("aave", async () => {
+  test("fortube", async () => {
     const result = await handler({ queryStringParameters: { project: "fortube", timestamp: "0", block: "12202080" } });
     expect(result).toMatchSnapshot();
     console.log(result);
