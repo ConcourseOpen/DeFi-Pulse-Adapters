ead1c827de622323890905ce6f9e9b6aa70ed9af
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-04-14 21:04:20 +0200

    Injective Protocol project added to the v2 DeFi Pulse indexer

---
+++ v2/projects/injective-protocol/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Injective Protocol",
+  symbol: "INJ",
+  category: "DEXes",
+  start: 1615707330,
+  tokenHolderMap: [
+    {
+      tokens: [
+        "0xe28b3b32b6c345a34ff64674606124dd5aceca30", // INJ
+      ],
+      holders: ["0x53f2b8cc450679d04c479a048dc3ff39a4D20D13"],
+    },
+  ],
+};
