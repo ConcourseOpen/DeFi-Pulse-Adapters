a3ed464e6fd50365566bf705304cb06e6a907a93
nedodn <odennate@gmail.com>
2021-04-22 09:42:39 -0500

    update warp adapter

--- v2/projects/warp/index.js
+++ v2/projects/warp/index.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Warp Finance",
   token: "WARP",
   category: "Lending",
-  start: 11803584,
+  start: 1610650220,
   tokenHolderMap: [
     {
       holders: {
@@ -10,10 +10,9 @@ module.exports = {
         logConfig: {
           target: "0x6C74E2A1074ABe18969Be37210B93e681A40b35A",
           topic: "NewLPVault(address)",
-          keys: ["topics"],
           fromBlock: 11803584,
         },
-        transform: (poolLog) => `0x${poolLog[1].slice(26)}`,
+        transform: (poolLog) => `0x${poolLog.substr(26, 40)}`,
       },
       tokens: {
         pullFromPools: true,
@@ -38,10 +37,63 @@ module.exports = {
         logConfig: {
           target: "0x6C74E2A1074ABe18969Be37210B93e681A40b35A",
           topic: "NewSCVault(address,address)",
-          keys: ["topics"],
           fromBlock: 11803584,
         },
-        transform: (poolLog) => `0x${poolLog[1].slice(26)}`,
+        transform: (poolLog) => `0x${poolLog.substr(26, 40)}`,
+      },
+      tokens: {
+        pullFromPools: true,
+        abi: {
+          inputs: [],
+          name: "stablecoin",
+          outputs: [
+            {
+              internalType: "contract ERC20",
+              name: "",
+              type: "address",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      },
+    },
+    {
+      holders: {
+        pullFromLogs: true,
+        logConfig: {
+          target: "0x8E0Fa7c5C7Fa86A059e865A90b50a90351df716a",
+          topic: "NewLPVault(address)",
+          fromBlock: 11654924,
+        },
+        transform: (poolLog) => `0x${poolLog.substr(26, 40)}`,
+      },
+      tokens: {
+        pullFromPools: true,
+        abi: {
+          inputs: [],
+          name: "LPtoken",
+          outputs: [
+            {
+              internalType: "contract IUniswapV2ERC20",
+              name: "",
+              type: "address",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      },
+    },
+    {
+      holders: {
+        pullFromLogs: true,
+        logConfig: {
+          target: "0x8E0Fa7c5C7Fa86A059e865A90b50a90351df716a",
+          topic: "NewSCVault(address,address)",
+          fromBlock: 11654924,
+        },
+        transform: (poolLog) => `0x${poolLog.substr(26, 40)}`,
       },
       tokens: {
         pullFromPools: true,
