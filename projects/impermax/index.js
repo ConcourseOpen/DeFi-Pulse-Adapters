160ae3902ea5dd37a6cd1a4c4657488dc08fa00a
nedodn <odennate@gmail.com>
2021-04-30 12:15:17 -0500

    fix impermax cat

--- projects/impermax/index.js
+++ projects/impermax/index.js
@@ -31,7 +31,7 @@ async function tvl(timestamp, block) {
 module.exports = {
   name: 'Impermax',
   token: null,
-  category: 'lending',
+  category: 'Lending',
   start: 1614643200, // 02/03/2021 @ 12:00am (UTC)
   tvl,
 };
