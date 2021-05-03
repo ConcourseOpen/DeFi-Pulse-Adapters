b8d99839afb08ec3c95e4fd32ff933f64b906c68
nedodn <odennate@gmail.com>
2021-05-03 15:39:53 -0500

    imx token

--- projects/impermax/index.js
+++ projects/impermax/index.js
@@ -30,7 +30,7 @@ async function tvl(timestamp, block) {
 
 module.exports = {
   name: 'Impermax',
-  token: null,
+  token: 'IMX',
   category: 'Lending',
   start: 1614643200, // 02/03/2021 @ 12:00am (UTC)
   tvl,
