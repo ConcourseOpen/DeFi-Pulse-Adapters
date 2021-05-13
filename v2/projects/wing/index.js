844c817e6e891639df608ef9e8cbf58e07fab32e
xcellsior <amiceli34@gmail.com>
2021-05-13 10:02:25 -0400

    Add wing Finance

--- v2/projects/wing/index.js
+++ v2/projects/wing/index.js
@@ -7,16 +7,28 @@ module.exports = {
   /* required for fetching token balances */
   tokenHolderMap: [
     {
-      tokens: [
-        '0xDb0f18081b505A7DE20B18ac41856BCB4Ba86A1a', // pWING
-        '0x0000000000000000000000000000000000000000', // ETH
-        '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', // WBTC
-        '0xcb46c550539ac3db72dc7af7c89b11c306c727c2', // pONT
-        '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-        '0x8F041A3940a5e6FB580075C3774E15FcFA0E1618', // oneWING
-      ],
-      holders: '0x2F9fa63066cfA2d727F57ddf1991557bA86F12c9',
-      checkETHBalance: true,
+      tokens: '0xDb0f18081b505A7DE20B18ac41856BCB4Ba86A1a', // pWING
+      holders: '0x4FA2425edf435B52016447Ab897d300d78c0Afe4',
+    },
+    {
+      tokens: '0x0000000000000000000000000000000000000000', // ETH
+      holders: '0xD93F4cf882D7d576a8Dc09e606B38CaF18Eda796',
+    },
+    {
+      tokens: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', // WBTC
+      holders: '0x3c22f604CC8b422F43becA8D8CdEF9922B96F454',
+    },
+    {
+      tokens: '0xcb46c550539ac3db72dc7af7c89b11c306c727c2', // pONT
+      holders: '0x65d999ddaaA6b0424BE37a53d5574E43e9433788',
+    },
+    {
+      tokens: '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
+      holders: '0x47dac542c5B9377E3A5d30992a6c32a3ad7f33db',
+    },
+    {
+      tokens: '0x8F041A3940a5e6FB580075C3774E15FcFA0E1618', // oneWING
+      holders: '0x363838fa35711EA3F8c8f95151203723cC6eE535',
     }
   ],
 };
