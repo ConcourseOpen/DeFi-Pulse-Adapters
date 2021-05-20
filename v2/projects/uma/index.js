32888f0757606b70568e98f7693b886f01844400
John Shutt <john.d.shutt@gmail.com>
2021-05-20 13:02:21 -0700

    feat(uma): get tokens and holders from financial contracts on-chain
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>

--- v2/projects/uma/index.js
+++ v2/projects/uma/index.js
@@ -6,23 +6,6 @@ module.exports = {
   /* required for fetching token balances */
   tokenHolderMap: [
     {
-      // tokens: [
-      //   '0xdac17f958d2ee523a2206206994597c13d831ec7',  // USDT
-      //   '0x6B175474E89094C44Da98b954EedeAC495271d0F',  // DAI
-      //   '0x514910771AF9Ca656af840dff83E8264EcF986CA',  // LINK
-      //   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',  // USDC
-      //   '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',  // WBTC
-      //   '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',  // MKR
-      // ],
-      holders: {
-        pullFromLogs: true,
-        logConfig: {
-          target: '0x3e532e6222afe9Bcf02DCB87216802c75D5113aE',
-          topic: 'NewContractRegistered(address,address,address[])',
-          fromBlock: 9937650,
-        },
-        transform: (poolLog) => `0x${poolLog.substr(26, 40)}`,
-      },
       tokens: {
         pullFromPools: true,
         abi: {
@@ -39,6 +22,16 @@ module.exports = {
           type: "function",
         },
       },
-    }
-  ]
-}
+      holders: {
+        pullFromLogs: true,
+        logConfig: {
+          target: '0x3e532e6222afe9Bcf02DCB87216802c75D5113aE',
+          topic: 'NewContractRegistered(address,address,address[])',
+          keys: ['topics'],
+          fromBlock: 9937650,
+        },
+        transform: (poolLog) => `0x${poolLog[1].slice(26)}`,
+      },
+    },
+  ],
+};
