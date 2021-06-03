fcec943f57f9de4c07b1c2add1a67f71b63b1658
hkhv <746392783@qq.com>
2021-06-03 17:30:25 +0800

    Add O3Swap adapter

---
+++ v2/projects/o3/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /* Project Metadata */
+  name: 'O3 Swap Token', // project name
+  token: 'O3', // null, or token symbol if project has a custom token
+  category: 'Assets', // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
+  start: 1614589793,
+  /* required for fetching token balances */
+  tokenHolderMap: [{
+    tokens: [
+      '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
+    ],
+    holders: [
+      '0x250e76987d838a75310c34bf422ea9f1AC4Cc906', // Treasury 1
+    ],
+  }],
+};
