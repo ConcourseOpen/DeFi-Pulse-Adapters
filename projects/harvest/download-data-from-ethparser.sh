cf4ded3074fddd88bb00bd1f36102c58b8d0bc44
Adam Becker <ad@mbecker.cc>
2021-06-06 22:00:00 -0700

    back on track

---
+++ projects/harvest/download-data-from-ethparser.sh
@@ -0,0 +1,4 @@
+# Get dependency JSON files from ethparser endpoint
+
+curl https://ethparser.herokuapp.com/contracts/vaults -s -o eth-vaults.json
+curl https://ethparser.herokuapp.com/contracts/pools -s -o eth-pools.json
--- testing/test-run.js
+++ testing/test-run.js
@@ -80,6 +80,7 @@
         let name = `${time}.json`;
 
         shell.mkdir('-p', path);
+        console.log(this.currentTest.value)
         fs.writeFileSync(`${path}/${name}`, JSON.stringify(this.currentTest.value, null, 2));
       });
     }
