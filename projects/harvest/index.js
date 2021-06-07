f9c492fdd36ce486b09c9f8434f422ef3b228ac5
Adam Becker <ad@mbecker.cc>
2021-06-06 23:03:27 -0700

    wow it works

--- projects/harvest/index.js
+++ projects/harvest/index.js
@@ -44,69 +44,68 @@
     'V_USDC_#V1'
   ]
 
-  const uniPools = [
-    'V_UNI_WETH_USDT_#V1',
-    'V_UNI_DAI_WETH_#V1',
-    'V_UNI_WBTC_WETH_#V1',
-    'V_SUSHI_WBTC_WETH',
-    'V_SUSHI_WBTC_WETH_#V1',
-    'V_SUSHI_WBTC_WETH_#V2',
-    'V_SUSHI_DAI_WETH',
-    'V_SUSHI_DAI_WETH_#V2',
-    'V_UNI_USDC_WETH',
-    'V_UNI_USDC_WETH_#V1',
-    'V_SUSHI_WETH_USDT',
-    'V_SUSHI_WETH_USDT_#V1',
-    'V_UNI_DPI_WETH',
-    'V_SUSHI_MIC_USDT',
-    'V_UNI_BASv2_DAI',
-    'V_SUSHI_MIS_USDT',
-    'V_SUSHI_USDC_WETH_#V2',
-    'V_UNI_DAI_BSGS',
-    'V_UNI_DAI_BSGS_#V1',
-    'V_UNI_DAI_BSG',
-    'V_UNI_DAI_BSG_#V1',
-    'V_UNI_DAI_BSG_#V2',
-    'V_1INCH_ETH_DAI',
-    'V_SUSHI_DAI_WETH_#V3',
-    'V_SUSHI_WETH_USDT_#V2',
-    'V_SUSHI_WBTC_WETH_#V2',
-    'V_UNI_mTSLA_UST_#V1',
-    'V_UNI_mTSLA_UST',
-    'V_UNI_BAC_DAI',
-    'V_UNI_UST_mAAPL',
-    'V_UNI_mAMZN_UST',
-    'V_UNI_mGOOGL_UST',
-    'V_SUSHI_SUSHI_WETH',
-    'V_UNI_WBTC_KLON',
-    'V_UNI_WBTC_KlonX',
-    'V_1INCH_ETH_1INCH',
-    'V_SUSHI_UST_WETH',
-    'V_UNI_UST_mTWTR',
-    'V_UNI_WBTC_KBTC',
-    'V_UNI_WBTC_KBTC_#V1',
-    'V_UNI_UST_mNFLX',
-    'V_UNI_ROPE20_WETH',
-    'V_UNI_DUDES20_WETH',
-    'V_UNI_WETH_MASK20',
-    'V_UNI_MUSE_WETH',
-    'V_CRV_usdp3CRV',
-    'V_1INCH_1INCH_WBTC',
-    'V_1INCH_1INCH_USDC',
-    'V_UNI_MVI_WETH',
-    'V_UNI_WETH_GPUNKS20',
-    'V_UNI_MEME20_WETH',
-    'V_UNI_KXUSD_DAI',
-    'V_SUSHI_PERP_WETH',
-    'V_BAC',
-    'V_UNI_COMFI_WETH',
+  const liquidityPools = [
+    // 'V_UNI_WETH_USDT_#V1',
+    // 'V_UNI_DAI_WETH_#V1',
+    // 'V_UNI_WBTC_WETH_#V1',
+    // 'V_SUSHI_WBTC_WETH',
+    // 'V_SUSHI_WBTC_WETH_#V1',
+    // 'V_SUSHI_WBTC_WETH_#V2',
+    // 'V_SUSHI_DAI_WETH',
+    // 'V_SUSHI_DAI_WETH_#V2',
+    // 'V_UNI_USDC_WETH',
+    // 'V_UNI_USDC_WETH_#V1',
+    // 'V_SUSHI_WETH_USDT',
+    // 'V_SUSHI_WETH_USDT_#V1',
+    // 'V_UNI_DPI_WETH',
+    // 'V_SUSHI_MIC_USDT',
+    // 'V_UNI_BASv2_DAI',
+    // 'V_SUSHI_MIS_USDT',
+    // 'V_SUSHI_USDC_WETH_#V2',
+    // 'V_UNI_DAI_BSGS',
+    // 'V_UNI_DAI_BSGS_#V1',
+    // 'V_UNI_DAI_BSG',
+    // 'V_UNI_DAI_BSG_#V1',
+    // 'V_UNI_DAI_BSG_#V2',
+    // 'V_1INCH_ETH_DAI',
+    // 'V_SUSHI_DAI_WETH_#V3',
+    // 'V_SUSHI_WETH_USDT_#V2',
+    // 'V_SUSHI_WBTC_WETH_#V2',
+    // 'V_UNI_mTSLA_UST_#V1',
+    // 'V_UNI_mTSLA_UST',
+    // 'V_UNI_BAC_DAI',
+    // 'V_UNI_UST_mAAPL',
+    // 'V_UNI_mAMZN_UST',
+    // 'V_UNI_mGOOGL_UST',
+    // 'V_SUSHI_SUSHI_WETH',
+    // 'V_UNI_WBTC_KLON',
+    // 'V_UNI_WBTC_KlonX',
+    // 'V_1INCH_ETH_1INCH',
+    // 'V_SUSHI_UST_WETH',
+    // 'V_UNI_UST_mTWTR',
+    // 'V_UNI_WBTC_KBTC',
+    // 'V_UNI_WBTC_KBTC_#V1',
+    // 'V_UNI_UST_mNFLX',
+    // 'V_UNI_ROPE20_WETH',
+    // 'V_UNI_DUDES20_WETH',
+    // 'V_UNI_WETH_MASK20',
+    // 'V_UNI_MUSE_WETH',
+    // 'V_CRV_usdp3CRV',
+    // 'V_1INCH_1INCH_WBTC',
+    // 'V_1INCH_1INCH_USDC',
+    // 'V_UNI_MVI_WETH',
+    // 'V_UNI_WETH_GPUNKS20',
+    // 'V_UNI_MEME20_WETH',
+    // 'V_UNI_KXUSD_DAI',
+    // 'V_SUSHI_PERP_WETH',
+    // 'V_BAC',
+    // 'V_UNI_COMFI_WETH',
     'V_UNI_WETH_MCAT20',
-    'V_ETHx5-USDC-1Jun21-LP',
-    'V_SUSHI_LFBTC_LIFT',
-    'V_SUSHI_WBTC_LFBTC',
-    'V_SUSHI_USDC_WETH',
-    'V_SUSHI_USDC_WETH_#V1'
-
+    // 'V_ETHx5-USDC-1Jun21-LP',
+    // 'V_SUSHI_LFBTC_LIFT',
+    // 'V_SUSHI_WBTC_LFBTC',
+    // 'V_SUSHI_USDC_WETH',
+    // 'V_SUSHI_USDC_WETH_#V1'
   ]
 
   function getDataByContractName(contractName) {
@@ -138,8 +137,38 @@
     ])
   }
 
+  async function getLiquidityPool(contractName, timestamp, block) {
+    const vault = getDataByContractName(contractName)
+
+    if (!vault) {
+      throw(`Error: ${contractName} not found in eth-vaults.json`)
+    }
+
+    console.log('yup')
+
+    // const [totalSupply, sharePrice, underlying] = await Promise.all([
+    //   sdk.api.abi.call({ block, target: vault.contract.address, abi: 'erc20:totalSupply', }),
+    //   sdk.api.abi.call({ block, target: vault.contract.address, abi: abi['fABISharePrice'], }),
+    //   sdk.api.abi.call({ block, target: vault.contract.address, abi: abi['fABIUnderlyingUnit'], })
+    // ])
+    //
+    // const underlyingCount = new BigNumber(totalSupply.output)
+    //   .times(new BigNumber(sharePrice.output))
+    //   .div(new BigNumber(underlying.output))
+    //
+    // return([
+    //   vault.underlying.address,
+    //   underlyingCount
+    // ])
+  }
+
   async function tvl(timestamp, block) {
     const assetAmounts = {}
+
+
+
+
+    // Single asset vaults
 
     const vals = await Promise.all(singleAssetVaults.map((contractName) => getSingleAssetVault(contractName, timestamp, block)))
 
@@ -147,6 +176,18 @@
       let existingVal = BigNumber(assetAmounts[val[0]] || 0)
       assetAmounts[val[0]] = BigNumber.sum(existingVal, val[1])
     })
+
+
+
+
+
+    // Liquidity pools
+
+
+    const vals = await Promise.all(liquidityPools.map((contractName) => getLiquidityPool(contractName, timestamp, block)))
+
+
+
 
     // Finally, convert the BigNums to strings
     const assetAmountStrings = {}
