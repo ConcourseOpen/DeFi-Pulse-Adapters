0fdcf87898488d0281093606a908d125cc529553
sameepsi <sameepsi@gmail.com>
2021-06-08 23:14:59 +0530

    Improved ABI in the adapter

--- chains/polygon/projects/quickswap/index.js
+++ chains/polygon/projects/quickswap/index.js
@@ -21,34 +21,34 @@ module.exports = {
           pullFromPools: true,
           abi: [
             {
-              "constant": true,
-              "inputs": [],
-              "name": "token1",
-              "outputs": [
+              constant: true,
+              inputs: [],
+              name: "token1",
+              outputs: [
                 {
-                  "internalType": "address",
-                  "name": "",
-                  "type": "address"
+                  internalType: "address",
+                  name: "",
+                  type: "address"
                 }
               ],
-              "payable": false,
-              "stateMutability": "view",
-              "type": "function"
+              payable: false,
+              stateMutability: "view",
+              type: "function"
             },
             {
-              "constant": true,
-              "inputs": [],
-              "name": "token0",
-              "outputs": [
+              constant: true,
+              inputs: [],
+              name: "token0",
+              outputs: [
                 {
-                  "internalType": "address",
-                  "name": "",
-                  "type": "address"
+                  internalType: "address",
+                  name: "",
+                  type: "address"
                 }
               ],
-              "payable": false,
-              "stateMutability": "view",
-              "type": "function"
+              payable: false,
+              stateMutability: "view",
+              type: "function"
             }
           ]
         }
