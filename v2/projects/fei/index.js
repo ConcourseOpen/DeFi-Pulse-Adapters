9c74e89c8dd0ceaedd449fd2faf4a817dd9ab1d7
Sebastian <sdalcega@gmail.com>
2021-06-24 14:20:38 -0700

    Add old PCV controller address

--- v2/projects/fei/index.js
+++ v2/projects/fei/index.js
@@ -17,6 +17,7 @@ module.exports = {
                 '0xa08A721dFB595753FFf335636674D76C455B275C', // EthReserveStabilizer.sol
                 '0xDa079A280FC3e33Eb11A78708B369D5Ca2da54fE', // EthPCVDripper.sol
                 '0x5d6446880fcd004c851ea8920a628c70ca101117', // EthUniswapPCVDepost.sol
+                '0x9b0C6299D08fe823f2C0598d97A1141507e4ad86', // EthUniswapPCVDeposit.sol (pre-FIP-5)
             ],
         },
   ],
