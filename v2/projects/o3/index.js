d1c77b6173f0ac3a15b0bc25dd95bdd9779a9594
hkhv <746392783@qq.com>
2021-06-30 13:46:51 +0800

    feat: O3Swap: add new pools

--- v2/projects/o3/index.js
+++ v2/projects/o3/index.js
@@ -9,6 +9,8 @@ module.exports = {
     tokens: [
       '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
       '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
+      '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+      '0xee9801669c6138e84bd50deb500827b776777d28', // O3
     ],
     holders: [
       '0x250e76987d838a75310c34bf422ea9f1AC4Cc906', // Treasury 1
