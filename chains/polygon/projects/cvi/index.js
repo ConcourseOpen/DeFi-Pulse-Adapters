9b0cbac5336bd844ea7ca18a08089612acbd5dba
Vlad Piliavsky <vladi@coti.io>
2021-06-30 12:18:24 +0300

    add CVI on Polygon

---
+++ chains/polygon/projects/cvi/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /* Project Metadata */
+  name: "CVI_Polygon",
+  website: "https://cvi.finance/",
+  token: "GOVI",
+  chain: "polygon",
+  category: "Derivatives",
+  start: 1622352129,
+  /* required for fetching token balances */
+  tokenHolderMap: [
+    {
+      tokens: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", // USDT
+      holders: "0x88D01eF3a4D586D5e4ce30357ec57B073D45ff9d", // USDTPlatform
+    },
+  ],
+};
