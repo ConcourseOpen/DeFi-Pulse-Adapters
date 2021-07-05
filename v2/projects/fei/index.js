b111c3549da1d1e5dd3cd3936b554299a5a1c985
Sebastian <sdalcega@gmail.com>
2021-07-05 10:49:00 -0700

    Add stETH PCV deposit

--- v2/projects/fei/index.js
+++ v2/projects/fei/index.js
@@ -10,6 +10,7 @@ module.exports = {
             checkETHBalance: true,
             tokens: [
                 '0x94b0a3d511b6ecdb17ebf877278ab030acb0a878', // FEI-ETH LP
+                '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', // stETH
             ],
             holders: [
                 '0xBFfB152b9392e38CdDc275D818a3Db7FE364596b', // GenesisGroup.sol
@@ -18,6 +19,7 @@ module.exports = {
                 '0xDa079A280FC3e33Eb11A78708B369D5Ca2da54fE', // EthPCVDripper.sol
                 '0x5d6446880fcd004c851ea8920a628c70ca101117', // EthUniswapPCVDepost.sol
                 '0x9b0C6299D08fe823f2C0598d97A1141507e4ad86', // EthUniswapPCVDeposit.sol (pre-FIP-5)
+                '0xAc38Ee05C0204A1E119C625d0a560D6731478880', // EthLidoPCVDeposit.sol
             ],
         },
   ],
