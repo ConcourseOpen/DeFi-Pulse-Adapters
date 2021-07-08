0cea7461bda55def4fb1c4e78acc27743a91cac6
nedodn <odennate@gmail.com>
2021-07-08 17:23:04 -0500

    merge

--- projects/balancer/index.js
+++ projects/balancer/index.js
@@ -5,8 +5,12 @@
   const sdk = require('../../sdk');
   const _ = require('underscore');
   const BigNumber = require('bignumber.js');
+  const { request, gql } = require("graphql-request");
 
   const abi = require('./abi');
+
+  const V2_ADDRESS = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
+  const tokensApi = 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2';
 
 /*==================================================
   TVL
@@ -71,6 +75,60 @@
       }
     });
 
+    try {
+      const POOL_TOKENS = gql`
+      {
+        balancers {
+          pools {
+            tokens {
+              address
+            }
+          }
+        }
+      }`;
+
+      const v2Tokens = await request(tokensApi, POOL_TOKENS, {
+        block,
+      });
+      let tokenAddresses = [];
+      for (let i = 0; i < v2Tokens.balancers[0].pools.length; i++) {
+        for (let address of v2Tokens.balancers[0].pools[i].tokens) {
+          tokenAddresses.push(address.address)
+        }
+      }
+      tokenAddresses = _.uniq(tokenAddresses);
+
+      let v2Calls = tokenAddresses.map((address) => {
+        return {
+          target: address,
+          params: V2_ADDRESS
+        }
+      });
+      let v2Balances = (await sdk.api.abi.multiCall({
+        block,
+        calls: v2Calls,
+        abi: 'erc20:balanceOf'
+      })).output;
+      _.each(v2Balances, (balanceOf) => {
+        if (balanceOf.success) {
+          let balance = balanceOf.output;
+          let address = balanceOf.input.target;
+
+          if (BigNumber(balance).toNumber() <= 0) {
+            return;
+          }
+
+          balances[address] = BigNumber(balances[address] || 0)
+            .plus(balance)
+            .toFixed();
+        }
+      });
+    } catch (e) {
+      console.log(e);
+      throw(e);
+    }
+
+
     return balances;
   }
 
@@ -80,7 +138,7 @@
 
   module.exports = {
     name: 'Balancer',
-    token: null,
+    token: 'BAL',
     category: 'dexes',
     start : 1582761600, // 02/27/2020 @ 12:00am (UTC)
     tvl
