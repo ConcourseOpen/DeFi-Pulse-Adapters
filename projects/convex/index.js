e819421c9192bf43f194295a8af697e0d35717c9
nedodn <odennate@gmail.com>
2021-07-08 21:02:05 -0500

    convex fixes

--- projects/convex/index.js
+++ projects/convex/index.js
@@ -123,7 +123,7 @@ async function tvl(timestamp, block) {
   sumSingleBalance(balances, crv, (await cvxCRVSupply).output)
   sumSingleBalance(balances, cvx, (await cvxStaked).output)
   console.log('convex end', balances)
-  return balances
+  return (await sdk.api.util.toSymbols(balances)).output;
 }
 
 
@@ -132,14 +132,14 @@ function sumSingleBalance(
   token,
   balance
 ) {
-    const prevBalance = ethers.BigNumber.from(balances[token] ?? "0");
+    const prevBalance = ethers.BigNumber.from(balances[token] || "0");
     balances[token] = prevBalance.add(ethers.BigNumber.from(balance)).toString();
 }
 
 module.exports = {
   name: 'Convex Finance', // project name
   token: "CVX",              // null, or token symbol if project has a custom token
-  category: 'assets',       // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
+  category: 'Assets',       // allowed values as shown on DefiPulse: 'Derivatives', 'DEXes', 'Lending', 'Payments', 'Assets'
   start: 1621296000,        // unix timestamp (utc 0) specifying when the project began, or where live data begins
   tvl                       // tvl adapter
 }
