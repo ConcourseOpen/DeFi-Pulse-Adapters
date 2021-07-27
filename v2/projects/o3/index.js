f05817072b7667df4c8e816e18b20cc8606a93c7
hkhv <746392783@qq.com>
2021-07-27 11:30:35 +0800

    feat: O3Swap: add new pools

--- v2/projects/o3/index.js
+++ v2/projects/o3/index.js
@@ -10,6 +10,7 @@ module.exports = {
       '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
       '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
       '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
       '0xee9801669c6138e84bd50deb500827b776777d28', // O3
     ],
     holders: [
