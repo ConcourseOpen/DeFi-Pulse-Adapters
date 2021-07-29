47c015abb8ed44b92daeae8c8cb5ae87817d8ce1
Vlad Piliavsky <vladi@coti.io>
2021-07-29 12:36:28 +0300

    added USDC platform

--- chains/polygon/projects/cvi/index.js
+++ chains/polygon/projects/cvi/index.js
@@ -12,5 +12,9 @@ module.exports = {
       tokens: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", // USDT
       holders: "0x88D01eF3a4D586D5e4ce30357ec57B073D45ff9d", // USDTPlatform
     },
+    {
+      tokens: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC
+      holders: "0x3863D0C9b7552cD0d0dE99fe9f08a32fED6ab72f", // USDCPlatform
+    },
   ],
 };
