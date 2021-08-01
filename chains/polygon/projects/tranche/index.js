3bf3cbce8897bb707fe3d51a60ba96936454fd7e
hg-tranche <70196848+hg-tranche@users.noreply.github.com>
2021-08-01 12:34:59 +0530

    added tranche polygon adapter

---
+++ chains/polygon/projects/tranche/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "TRANCHE-POLYGON",
+    token: "SLICE",
+    chain: 'polygon',
+    category: "Derivatives",
+    website: "https://tranche.finance/",
+    start: 1625640852,
+    tokenHolderMap: [
+        {
+            tokens: [
+                "0x27f8d03b3a2196956ed754badc28d73be8830a6e", //amDAI
+                "0x1a13f4ca1d028320a707d99520abfefca3998b7f", //amUSDC
+                "0x8df3aad3a84da6b69a4da8aec3ea40d9091b2ac4" //amWmatic
+            ],
+            holders: '0x03f44E563dD447449F48f8103b5dF70aFf7CF577'
+        }
+    ]
+};
+
+
