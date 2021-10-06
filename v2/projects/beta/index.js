c79c91aefe35b2c75a23d53832fecc4603ab6971
xcellsior <amiceli34@gmail.com>
2021-10-06 19:43:15 -0400

    beta

--- v2/projects/beta/index.js
+++ v2/projects/beta/index.js
@@ -34,7 +34,7 @@ module.exports = {
         },
       },
       {
-        holders: ["0x972a785b390D05123497169a04c72dE652493BE1"],
+        holders: "0x972a785b390D05123497169a04c72dE652493BE1",
         tokens: [
           "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
           "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
