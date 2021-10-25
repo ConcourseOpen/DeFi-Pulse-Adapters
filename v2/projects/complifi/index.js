ccfd60109d4eff7bee4d19d20fc296dc60658044
xcellsior <34947311+xcellsior@users.noreply.github.com>
2021-10-25 15:42:33 -0400

    hard code complifi

--- v2/projects/complifi/index.js
+++ v2/projects/complifi/index.js
@@ -1,21 +1,28 @@
-const axios = require('axios');
-
 module.exports = {
     name: 'CompliFi',
     token: 'COMFI',
     category: 'Derivatives',
     start: 1616484124, // Mar-23-2021 07:22:04 AM +UTC
+    //chain: 'Multichain,Ethereum-Polygon',
     tokenHolderMap: [
       {
-        tokens: async () => {
-          return (await axios.get('https://eth.complifi.me/api/protocol/pulse?networkId=1')).data.tokens || [];
-        },
-        holders: async () => {
-          return (await axios.get('https://eth.complifi.me/api/protocol/pulse?networkId=1')).data.holders || [];
-        },
-      }
+        tokens: [
+          "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+        ],
+        holders: [
+        "0xd4C10158e9B7832e0Dd7C121Ce9eAe0856E7F6D9",
+        "0x37F14Bcd72076C4F7DdD7fd29DcC519fABc935F7",
+        "0xac72F4Ec02EdF994C18b208B99C2ffBE505418c7",
+        "0x2E17509724dbE94D9cB03c3ecDaB6a49bC78AF94",
+        "0x817e30fD53b2fE30F87A4Dc450811029961bAbF9",
+        "0xc8D05aC9bE57779b0F46AC12313567f7359A8495",
+        "0xD6f361320E61D0A912AcF10c53e2D5C43aa79dc2",
+        "0xea5b9650f6c47D112Bb008132a86388B594Eb849",
+        "0x4eba099F97ffeD8de47a14f835820815f141Ea08",
+        "0xd498bF281262e04b0Dc8A1c6D14877Cee46AAAAE"
+        ]
+      },
     ],
   };
-
 
 
