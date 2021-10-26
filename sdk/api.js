e12b4ba97e8b07bc8d2bba90e607394e42ee7245
xcellsior <34947311+xcellsior@users.noreply.github.com>
2021-10-26 18:32:18 -0400

    Update api.js

--- sdk/api.js
+++ sdk/api.js
@@ -2,13 +2,13 @@
   Modules
   ==================================================*/
 
-  require('dotenv').config();
-  const axios = require('axios');
-  const _ = require('underscore');
-  const utility = require('./util');
-  const Bottleneck = require('bottleneck');
-  const term = require( 'terminal-kit' ).terminal;
-  const $indexerHost = 'http://defipulse-indexer-sync-server-staging.us-west-1.elasticbeanstalk.com';
+require('dotenv').config();
+const axios = require('axios');
+const _ = require('underscore');
+const utility = require('./util');
+const Bottleneck = require('bottleneck');
+const term = require( 'terminal-kit' ).terminal;
+const $indexerHost = 'http://defipulse-indexer-sync-server-staging.us-west-1.elasticbeanstalk.com';
 
 /*==================================================
   Helper Methods
@@ -54,79 +54,79 @@
           });
         }
 
-        function processRequest(chunk) {
-          return new Promise(async(resolve, reject) => {
-            try {
-              let opts = {
-                ...options
-              }
-              opts[options.chunk.param] = chunk;
-
-
-              let call = await POST(endpoint, opts);
-              complete++;
-
-              if(process.env.LOG_PROGRESS == 'true') {
-                progressBar.update(complete / chunks.length);
-              }
-
-              if(call.ethCallCount) {
-                ethCallCount += call.ethCallCount;
-
-                if(options.chunk.combine == 'array') {
-                  output = [
-                    ...output,
-                    ...call.output
-                  ]
-                } else if(options.chunk.combine == 'balances') {
-                  output.push(call.output);
-                }
-              }
-              resolve();
-            } catch(error) {
-              reject(error);
+      function processRequest(chunk) {
+        return new Promise(async(resolve, reject) => {
+          try {
+            let opts = {
+              ...options
             }
-          });
-        }
+            opts[options.chunk.param] = chunk;
 
-        const limiter = new Bottleneck({
-          maxConcurrent: process.env.ADAPTER_CONCURRENCY || 1
+
+            let call = await POST(endpoint, opts);
+            complete++;
+
+            if(process.env.LOG_PROGRESS == 'true') {
+              progressBar.update(complete / chunks.length);
+            }
+
+            if(call.ethCallCount) {
+              ethCallCount += call.ethCallCount;
+
+              if(options.chunk.combine == 'array') {
+                output = [
+                  ...output,
+                  ...call.output
+                ]
+              } else if(options.chunk.combine == 'balances') {
+                output.push(call.output);
+              }
+            }
+            resolve();
+          } catch(error) {
+            reject(error);
+          }
         });
-
-        await Promise.all(_.map(chunks, (chunk) => {
-          return limiter.schedule(() => {
-            return processRequest(chunk);
-          });
-        }));
-
-        if(process.env.LOG_PROGRESS == 'true') {
-          progressBar.update(1);
-        }
-
-        if(options.chunk.combine == 'balances') {
-          console.log('balances combine')
-          output = utility.sum(output);
-        }
-
-        return {
-          ethCallCount,
-          output
-        }
-      } else {
-        let url = `${process.env.DEFIPULSE_API_URL}/${process.env.DEFIPULSE_KEY}${endpoint}`;
-
-        if(process.env.INFURA_KEY) {
-          url = `${url}?infura-key=${process.env.INFURA_KEY}`;
-        }
-
-        let response = await axios.post(url, options);
-
-        return response.data;
       }
-    } catch(error) {
-      throw error.response ? error.response.data : error;
+
+      const limiter = new Bottleneck({
+        maxConcurrent: process.env.ADAPTER_CONCURRENCY || 1
+      });
+
+      await Promise.all(_.map(chunks, (chunk) => {
+        return limiter.schedule(() => {
+          return processRequest(chunk);
+        });
+      }));
+
+      if(process.env.LOG_PROGRESS == 'true') {
+        progressBar.update(1);
+      }
+
+      if(options.chunk.combine == 'balances') {
+        console.log('balances combine')
+        output = utility.sum(output);
+      }
+
+      return {
+        ethCallCount,
+        output
+      }
+    } else {
+      let url = `${process.env.DEFIPULSE_API_URL}/${process.env.DEFIPULSE_KEY}${endpoint}`;
+
+      if(process.env.INFURA_KEY) {
+        url = `${url}?infura-key=${process.env.INFURA_KEY}`;
+      }
+
+      let response = await axios.post(url, options);
+
+      return response.data;
     }
+  } catch(error) {
+    throw error.response ? error.response.data : error;
   }
+}
 
 /**
  *
@@ -138,7 +138,6 @@
  * @private
  */
 async function _testAdapter(block, timestamp, project, tokenBalanceMap) {
-  project.name = project.testName;
   project = JSON.stringify(project, _jsonConverter, 2);
 
   try {
@@ -163,6 +162,29 @@ async function _testAdapter(block, timestamp, project, tokenBalanceMap) {
 /**
  *
  * @param {Number} timestamp
+ * @param {String} depositor
+ * @returns {Promise<*>}
+ * @private
+ */
+async function _testStakingAdapter(timestamp, depositor) {
+
+  try {
+    return (
+      await axios({
+        method: 'GET',
+        url: `${$indexerHost}/test-eth2.0-staking?timestamp=${timestamp}&depositor=${depositor}`,
+      })
+    ).data;
+  } catch(error) {
+    console.error(`Error: ${error.response ? error.response.data : error}`);
+    throw error.response ? error.response.data : error;
+  }
+}
+
+
+/**
+ *
+ * @param {Number} timestamp
  * @param {String} chain
  * @returns {Promise<*>}
  * @private
@@ -182,64 +204,61 @@ async function _lookupBlock(timestamp, chain) {
     return POST(`/erc20/${endpoint}`, options);
   }
 
-  async function eth(endpoint, options) {
-    return POST(`/eth/${endpoint}`, options);
-  }
+async function eth(endpoint, options) {
+  return POST(`/eth/${endpoint}`, options);
+}
 
-  async function util(endpoint, options) {
-    return POST(`/util/${endpoint}`, options);
-  }
+async function util(endpoint, options) {
+  return POST(`/util/${endpoint}`, options);
+}
 
-  async function abi(endpoint, options) {
-    return POST(`/abi/${endpoint}`, options);
-  }
+async function abi(endpoint, options) {
+  return POST(`/abi/${endpoint}`, options);
+}
 
-  async function maker(endpoint, options) {
-    return POST(`/cdp/maker/${endpoint}`, options);
-  }
+async function maker(endpoint, options) {
+  return POST(`/cdp/maker/${endpoint}`, options);
+}
 
-  async function compound(endpoint, options) {
-    return POST(`/cdp/compound/${endpoint}`, options);
-  }
+async function compound(endpoint, options) {
+  return POST(`/cdp/compound/${endpoint}`, options);
+}
 
-  async function aave(endpoint, options) {
-    return POST(`/cdp/aave/${endpoint}`, options);
-  }
+async function aave(endpoint, options) {
+  return POST(`/cdp/aave/${endpoint}`, options);
+}
 
-  async function cdp(endpoint, options) {
-    return POST(`/cdp/${endpoint}`, options);
-  }
+async function cdp(endpoint, options) {
+  return POST(`/cdp/${endpoint}`, options);
+}
 
 /*==================================================
   Exportsd
   ==================================================*/
 
-  module.exports = {
-    abi: {
-      call: (options) => abi('call', { ...options }),
-      multiCall: (options) => abi('multiCall', { ...options, chunk: {param: 'calls', length: 2500, combine: 'array'} })
+module.exports = {
+  abi: {
+    call: (options) => abi('call', { ...options }),
+    multiCall: (options) => abi('multiCall', { ...options, chunk: {param: 'calls', length: 5000, combine: 'array'} })
+  },
+  cdp: {
+    getAssetsLocked: (options) => cdp('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 1000, combine: 'balances'} }),
+    maker: {
+      tokens: (options) => maker('tokens', { ...options }),
+      getAssetsLocked: (options) => maker('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 3000, combine: 'balances'} })
     },
-    cdp: {
-      getAssetsLocked: (options) => cdp('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 1000, combine: 'balances'} }),
-      maker: {
-        tokens: (options) => maker('tokens', { ...options }),
-        getAssetsLocked: (options) => maker('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 3000, combine: 'balances'} })
-      },
-      compound: {
-        tokens: (options) => compound('tokens', { ...options }),
-        getAssetsLocked: (options) => compound('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 1000, combine: 'balances'} })
-      },
-      aave: {
-        getAssetsLocked: (options) => aave('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 1000, combine: 'balances'} })
-      }
+    compound: {
+      tokens: (options) => compound('tokens', { ...options }),
+      getAssetsLocked: (options) => compound('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 1000, combine: 'balances'} })
     },
     util: {
       getLogs: (options) => util('getLogs', { ...options }),
+      supportedTokens: () => util('supportedTokens'),
       tokenList: () => util('tokenList'),
       kyberTokens: () => util('kyberTokens'),
       getEthCallCount: () => util('getEthCallCount'),
       resetEthCallCount: () => util('resetEthCallCount'),
-      toSymbols: (data) => util('toSymbols', { data }),
+      toSymbols: (data, chain=null) => util('toSymbols', { data, chain }),
       unwrap: (options) => util('unwrap', { ...options }),
       lookupBlock: _lookupBlock,
       /**
@@ -255,6 +274,15 @@ async function _lookupBlock(timestamp, chain) {
       }),
       /**
        *
+       * @param {Number} timestamp
+       * @param {String} depositor
+       * @returns {Promise<*>}
+       */
+      testStakingAdapter: ((timestamp, depositor) => {
+        return _testStakingAdapter(timestamp, depositor);
+      }),
+      /**
+       *
        */
       isCallable: _isCallable,
       /**
@@ -264,15 +292,62 @@ async function _lookupBlock(timestamp, chain) {
        */
       isString: (str) => typeof str === 'string',
     },
-    eth: {
-      getBalance: (options) => eth('getBalance', options),
-      getBalances: (options) => eth('getBalances', options),
-    },
-    erc20: {
-      info: (target) => erc20('info', { target }),
-      symbol: (target) => erc20('symbol', { target }),
-      decimals: (target, chain=null) => erc20('decimals', { target, chain }),
-      totalSupply: (options) => erc20('totalSupply', { ...options }),
-      balanceOf: (options) => erc20('balanceOf', { ...options }),
+    aave: {
+      getAssetsLocked: (options) => aave('getAssetsLocked', { ...options, chunk: {param: 'targets', length: 1000, combine: 'balances'} })
     }
+  },
+  util: {
+    getLogs: (options) => util('getLogs', { ...options }),
+    supportedTokens: () => util('supportedTokens'),
+    tokenList: () => util('tokenList'),
+    kyberTokens: () => util('kyberTokens'),
+    getEthCallCount: () => util('getEthCallCount'),
+    resetEthCallCount: () => util('resetEthCallCount'),
+    toSymbols: (data, chain=null) => util('toSymbols', { data, chain }),
+    unwrap: (options) => util('unwrap', { ...options }),
+    lookupBlock: (timestamp) => util('lookupBlock', { timestamp }),
+    /**
+     *
+     * @param {Number} block
+     * @param {Number} timestamp
+     * @param {Object} project
+     * @param {Object} tokenBalanceMap
+     * @returns {Promise<*>}
+     */
+    testAdapter: ((block, timestamp, project, tokenBalanceMap) => {
+      return _testAdapter(block, timestamp, project, tokenBalanceMap);
+    }),
+    /**
+     *
+     * @param {Number} timestamp
+     * @param {String} project
+     * @returns {Promise<*>}
+     */
+    testStakingAdapter: ((timestamp, depositor) => {
+      return _testStakingAdapter(timestamp, depositor);
+    }),
+    /**
+     *
+     * @param {function} func
+     * @returns {boolean}
+     */
+    isCallable: (func) => typeof func === 'function',
+    /**
+     *
+     * @param {String} str
+     * @returns {boolean}
+     */
+    isString: (str) => typeof str === 'string',
+  },
+  eth: {
+    getBalance: (options) => eth('getBalance', options),
+    getBalances: (options) => eth('getBalances', options),
+  },
+  erc20: {
+    info: (target) => erc20('info', { target }),
+    symbol: (target) => erc20('symbol', { target }),
+    decimals: (target) => erc20('decimals', { target }),
+    totalSupply: (options) => erc20('totalSupply', { ...options }),
+    balanceOf: (options) => erc20('balanceOf', { ...options }),
   }
+}
