c38e6c5b97137f954c903b08a71bd1c4fd064bde
frank-beard <odennate@gmail.com>
2021-11-15 13:38:46 -0600

    update tranche

--- v2/projects/tranche/index.js
+++ v2/projects/tranche/index.js
@@ -11,12 +11,17 @@ module.exports = {
     /* required for fetching token balances */
     tokenHolderMap: [
         {
-            tokens: async () => {
-                return (await axios.get('https://api.tranche.finance/api/v1/common/token-address?network=ethereum')).data.result || [];
-            },
-            holders: async () => {
-                return (await axios.get('https://api.tranche.finance/api/v1/common/holder-address?network=ethereum')).data.result || [];
-            },
+            tokens: [
+                '0x39aa39c021dfbae8fac545936693ac917d5e7563',
+                '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
+                '0x0AeE8703D34DD9aE107386d3eFF22AE75Dd616D1',
+                '0xccf4429db6322d5c611ee964527d42e5d685dd6a',
+                '0xface851a4921ce59e912d19329929ce6da6eb0c7'
+            ],
+            holders: [
+                '0x05060F5ab3e7A98E180B418A96fFc82A85b115e7',
+                '0xAB4235a9ACf00A45557E90F7dB127f3b293cA45A'
+            ]
         }
     ]
 };
