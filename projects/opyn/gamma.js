f9ef110275b21219f069526babe88669d8f84cdf
Daniele Ugolini <dan.ugolini@gmail.com>
2021-11-18 21:37:27 +0100

    added sdcrvWSBTC as wbtc (#453)

--- projects/opyn/gamma.js
+++ projects/opyn/gamma.js
@@ -15,12 +15,18 @@ const isWhitelistedCollateral = require('./abis/gamma/isWhitelistedCollateral.js
 const START_BLOCK = 11551118;
 const whitelist = "0xa5ea18ac6865f315ff5dd9f1a7fb1d41a30a6779";
 const marginPool = "0x5934807cc0654d46755ebd2848840b616256c6ef";
+const yvUSDC = "0x5f18c75abdae578b483e5f43f12a39cf75b973a9";
+const usdc = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+const sdeCRV = "0xa2761B0539374EB7AF2155f76eb09864af075250".toLowerCase();
+const sdcrvWSBTC = "0x24129b935aff071c4f0554882c0d9573f4975fed".toLowerCase();
+const WBTC = '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'.toLowerCase();
+const ETH = '0x0000000000000000000000000000000000000000'.toLowerCase();
 
 /*==================================================
   TVL
 ==================================================*/
 
-module.exports = async function tvl(timestamp, block) {  
+module.exports = async function tvl(timestamp, block) {
   let balances = {};
 
   if(block >= START_BLOCK) {
@@ -31,11 +37,11 @@ module.exports = async function tvl(timestamp, block) {
         .tokenList()
         .then((supportedTokens) => supportedTokens.map(({ contract }) => contract))
     );
-    
+
     // get ETH balance
     const balance = (await sdk.api.eth.getBalance({target: marginPool, block})).output;
     balances["0x0000000000000000000000000000000000000000"] = BigNumber(balances["0x0000000000000000000000000000000000000000"] || 0).plus(BigNumber(balance)).toFixed();
-  
+
     await Promise.all(supportedTokens.map(async (pulseToken) => {
       const collateralAsset = pulseToken.toLocaleLowerCase();
 
@@ -48,7 +54,7 @@ module.exports = async function tvl(timestamp, block) {
             block
           })
         ).output;
-    
+
         if(isWhitelistedCollateralToken) {
           const balanceOfResult = (
             await sdk.api.abi.call({
@@ -59,10 +65,47 @@ module.exports = async function tvl(timestamp, block) {
             })
           ).output;
 
-          balances[collateralAsset] = BigNumber(balances[collateralAsset] || 0).plus(BigNumber(balanceOfResult)).toFixed();          
+          balances[collateralAsset] = BigNumber(balances[collateralAsset] || 0).plus(BigNumber(balanceOfResult)).toFixed();
         }
       }
     }));
+
+    // Add yvUSDC as USDC to balances
+    const yvUSDCBalance = (
+      await sdk.api.abi.call({
+        target: yvUSDC,
+        params: marginPool,
+        abi: 'erc20:balanceOf',
+        block
+      })
+    ).output;
+
+    balances[usdc] = BigNumber(balances[usdc] || 0).plus(BigNumber(yvUSDCBalance)).toFixed();
+
+    // Add sdeCRV as ETH to balances
+    const sdeCRVBalance = (
+      await sdk.api.abi.call({
+        target: sdeCRV,
+        params: marginPool,
+        abi: 'erc20:balanceOf',
+        block
+      })
+    ).output;
+
+    balances[ETH] = BigNumber(balances[ETH] || 0).plus(BigNumber(sdeCRVBalance)).toFixed();
+
+    // Add sdcrvWSBTC as WBTC to balances
+    const sdcrvWSBTCBalance = (
+      await sdk.api.abi.call({
+        target: sdcrvWSBTC,
+        params: marginPool,
+        abi: 'erc20:balanceOf',
+        block
+      })
+    ).output;
+
+    balances[WBTC] = BigNumber(balances[WBTC] || 0).plus(BigNumber(sdcrvWSBTCBalance)).toFixed();
+
   }
 
   return balances;
