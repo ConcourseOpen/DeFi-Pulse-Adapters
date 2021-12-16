c2a90bc5d272c6b9db36e1ea282a45ed2242da93
xcellsior <amiceli34@gmail.com>
2021-12-16 18:46:53 -0500

    change kyber to use uni-v3 type of adapter

--- chains/polygon/projects/kyber/index.js
+++ chains/polygon/projects/kyber/index.js
@@ -1,21 +1,22 @@
-const BigNumber = require('bignumber.js');
 
 const sdk = require('../../../../sdk');
-const token0 = require('./abis/token0.json');
-const token1 = require('./abis/token1.json');
-const getReserves = require('./abis/getReserves.json');
 
 const START_BLOCK = 15968294;
 const FACTORY = '0x5f1fe642060b5b9658c15721ea22e982643c095c';
 
 async function tvl(timestamp, block) {
-  const supportedTokens = await (
+  let supportedTokens = await (
     sdk
       .api
       .util
-      .tokenList()
-      .then((supportedTokens) => supportedTokens.map(({ contract }) => contract))
+      .supportedTokens()
+      .then((supportedTokens) => supportedTokens.map((token) => {
+        if (token.platforms && token.platforms['polygon-pos']) {
+          return token.platforms['polygon-pos'];
+        }
+      }))
   );
+  supportedTokens = supportedTokens.filter(token => token)
 
   const logs = (
     await sdk.api.util
@@ -29,113 +30,71 @@ async function tvl(timestamp, block) {
       })
   ).output;
 
-  const pairAddresses = logs
-    // sometimes the full log is emitted
-    .map((log) =>
-      typeof log === 'string' ? log : `0x${log.data.slice(64 - 40 + 2, 64 + 2)}`
-    )
-    // lowercase
-    .map((pairAddress) => pairAddress.toLowerCase());
+  let pairAddresses = []
+  const token0Addresses = []
+  const token1Addresses = []
+  for (let log of logs) {
+    token0Addresses.push(`0x${log.topics[1].substr(-40)}`.toLowerCase())
+    token1Addresses.push(`0x${log.topics[2].substr(-40)}`.toLowerCase())
+  }
 
-  const [token0Addresses, token1Addresses] = await Promise.all([
-    sdk.api.abi
-      .multiCall({
-        abi: token0,
-        calls: pairAddresses.map((pairAddress) => ({
-          target: pairAddress,
-        })),
-        block,
-        chain: 'polygon'
-      })
-      .then(({ output }) => output),
-    sdk.api.abi
-      .multiCall({
-        abi: token1,
-        calls: pairAddresses.map((pairAddress) => ({
-          target: pairAddress,
-        })),
-        block,
-        chain: 'polygon'
-      })
-      .then(({ output }) => output),
-  ]);
+  pairAddresses = (logs.map((log) =>         // sometimes the full log is emitted
+    typeof log === 'string' ? log.toLowerCase() : `0x${log.data.slice(64 - 40 + 2, 64 + 2)}`.toLowerCase()));
 
-  const pairs = {};
+
+  const pairs = {}
   // add token0Addresses
-  token0Addresses.forEach((token0Address) => {
-    if (token0Address.success) {
-      const tokenAddress = token0Address.output.toLowerCase();
-
-      if (supportedTokens.includes(tokenAddress)) {
-        const pairAddress = token0Address.input.target.toLowerCase();
-        pairs[pairAddress] = {
-          token0Address: tokenAddress,
-        }
+  token0Addresses.forEach((token0Address, i) => {
+    if (supportedTokens.includes(token0Address)) {
+      const pairAddress = pairAddresses[i]
+      pairs[pairAddress] = {
+        token0Address: token0Address,
       }
     }
-  });
+  })
 
   // add token1Addresses
-  token1Addresses.forEach((token1Address) => {
-    if (token1Address.success) {
-      const tokenAddress = token1Address.output.toLowerCase();
-      if (supportedTokens.includes(tokenAddress)) {
-        const pairAddress = token1Address.input.target.toLowerCase();
-        pairs[pairAddress] = {
-          ...(pairs[pairAddress] || {}),
-          token1Address: tokenAddress,
-        }
+  token1Addresses.forEach((token1Address, i) => {
+    if (supportedTokens.includes(token1Address)) {
+      const pairAddress = pairAddresses[i]
+      pairs[pairAddress] = {
+        ...(pairs[pairAddress] || {}),
+        token1Address: token1Address,
       }
     }
-  });
+  })
 
-  const reserves = (await sdk.api.abi
-    .multiCall({
-      abi: getReserves,
-      chain: 'polygon',
-      calls: Object.keys(pairs).map((pairAddress) => ({
-        target: pairAddress,
-      })),
+  let balanceCalls = []
+
+  for (let pair of Object.keys(pairs)) {
+    if (pairs[pair].token0Address) {
+      balanceCalls.push({
+        target: pairs[pair].token0Address,
+        params: pair,
+      })
+    }
+
+    if (pairs[pair].token1Address) {
+      balanceCalls.push({
+        target: pairs[pair].token1Address,
+        params: pair,
+      })
+    }
+  }
+
+  const tokenBalances = (
+    await sdk.api.abi.multiCall({
+      abi: 'erc20:balanceOf',
+      calls: balanceCalls,
       block,
-    })).output;
-  let balances;
-  balances = reserves.reduce((accumulator, reserve, i) => {
-    if (reserve.success) {
-      const pairAddress = reserve.input.target.toLowerCase();
-      const pair = pairs[pairAddress] || {};
+      chain: 'polygon'
+    })
+  )
 
-      // handle reserve0
-      if (pair.token0Address) {
-        const reserve0 = new BigNumber(reserve.output['0']);
-        if (!reserve0.isZero()) {
-          const existingBalance = new BigNumber(
-            accumulator[pair.token0Address] || '0'
-          );
+  let balances = {};
 
-          accumulator[pair.token0Address] = existingBalance
-            .plus(reserve0)
-            .toFixed()
-        }
-      }
+  sdk.util.sumMultiBalanceOf(balances, tokenBalances)
 
-      // handle reserve1
-      if (pair.token1Address) {
-        const reserve1 = new BigNumber(reserve.output['1']);
-
-        if (!reserve1.isZero()) {
-          const existingBalance = new BigNumber(
-            accumulator[pair.token1Address] || '0'
-          );
-
-          accumulator[pair.token1Address] = existingBalance
-            .plus(reserve1)
-            .toFixed()
-        }
-      }
-    }
-
-    return accumulator
-  }, {})
   return balances;
 }
 
