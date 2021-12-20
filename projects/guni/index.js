4de6dac42cdf78594965c4e0295be6cf1b1aecc8
kassandra <0xdada@protonmail.com>
2021-12-20 10:21:26 -0500

    add g-uni adapter

---
+++ projects/guni/index.js
@@ -0,0 +1,65 @@
+const sdk = require('../../sdk');
+const abi = require("./abi.json");
+
+const G_UNI_Factory = "0xEA1aFf9dbFfD1580F6b81A3ad3589E66652dB7D9";
+
+const tvl = async (timestamp, block, chainBlocks) => {
+  const getAllDeplores = (
+    await sdk.api.abi.call({
+      abi: abi.getDeployers,
+      target: G_UNI_Factory,
+      block,
+    })
+  ).output;
+
+  const getAllPools = (
+    await sdk.api.abi.multiCall({
+      abi: abi.getPools,
+      calls: getAllDeplores.map((deployer) => ({
+        target: G_UNI_Factory,
+        params: deployer,
+      })),
+      block,
+    })
+  ).output.map((pool) => pool.output);
+
+  const allGelatoPools = [].concat.apply([], getAllPools);
+
+  const token0 = (
+    await sdk.api.abi.multiCall({
+      abi: abi.token0,
+      calls: allGelatoPools.map((pool) => ({
+        target: pool,
+      })),
+      block,
+    })
+  ).output.map((t0) => t0.output);
+
+  const token1 = (
+    await sdk.api.abi.multiCall({
+      abi: abi.token1,
+      calls: allGelatoPools.map((pool) => ({
+        target: pool,
+      })),
+      block,
+    })
+  ).output.map((t1) => t1.output);
+
+  const balanceOfPools = (
+    await sdk.api.abi.multiCall({
+      abi: abi.getUnderlyingBalances,
+      calls: allGelatoPools.map((pool) => ({
+        target: pool,
+      })),
+      block,
+    })
+  ).output.map((bal) => bal.output);
+  
+  let balanceArray = [];
+  for (let i = 0; i < allGelatoPools.length; i++) {
+    balanceArray.push(balanceOfPools[i].amount0Current, token0[i]);
+    balanceArray.push(balanceOfPools[i].amount1Current, token1[i]);
+  }
+
+  return sdk.util.sum(balanceArray)
+}
