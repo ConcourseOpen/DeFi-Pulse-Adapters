d4299497aa59db98de5af9aec7bb6e3ecaceec7a
Greg Frasco <frascog17@gmail.com>
2021-12-27 16:32:28 -0500

    fix: category

--- projects/truefi/index.js
+++ projects/truefi/index.js
@@ -68,7 +68,7 @@ module.exports = {
   name: 'TrueFi',               // project name
   website: 'https://app.truefi.com',
   token: 'TRU',
-  category: 'Lending',          // Lending
+  category: 'lending',          // Lending
   start: 1605830400,            // 11/20/2020 @ 12:00am (UTC)
   tvl                           // tvl adapter
 }
