ad501f4d61af8a92800b65b5f55140d0813cf3ea
Adam Feather <dakramystic@sirenmarkets.com>
2021-12-27 09:41:44 -0800

    updated siren tvl calc

---
+++ chains/polygon/projects/siren/index.js
@@ -0,0 +1,153 @@
+/*==================================================
+  Modules
+  ==================================================*/
+  const { request, gql } = require("graphql-request");
+  const BigNumber = require('bignumber.js');
+  const axios = require('axios');
+  const sdk = require('../../../../sdk');
+
+
+  const POLYGON_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/sirenmarkets/protocol-v2-matic'
+
+/*==================================================
+  TVL
+  ==================================================*/
+
+  async function getTokens( amms) {
+
+
+    let tokens =[];
+    for (let i = 0; i < amms['amms'].length; i++) {
+      let amm = amms['amms'][i]
+      tokens.push(amm['collateralToken']['id']);
+
+    }
+    return tokens;
+  }
+
+  async function getHolders( amms) {
+    
+    // const {seriesVaults} = await request(POLYGON_GRAPH_URL, GET_SERIES_VAULT_ID);
+    const {seriesVaults} = await request(POLYGON_GRAPH_URL, GET_SERIES_VAULT_ID);
+
+    let holders =[];
+
+    for (let i = 0; i < amms['amms'].length; i++) {
+      let amm = amms['amms'][i]
+      holders.push(amm['id']);
+
+    }
+    holders.push(seriesVaults[0]['id']);
+    return holders;
+  }
+
+  async function calculatePolygonTVL(timestamp,  block, polygon_amms) {
+
+    const {seriesVaults} = await request(POLYGON_GRAPH_URL, GET_SERIES_VAULT_ID);
+
+    const polygon_result = {
+      'polygon:0x2791bca1f2de4661ed88a30c99a7a9449aa84174': '0'
+    };
+  
+    for (let i = 0; i < polygon_amms['amms'].length; i++) {
+      const amm = polygon_amms['amms'][i];
+      const collateralToken = amm['collateralToken']['id'];
+  
+      // Get collateral in AMM
+      const response = await sdk.api.abi.call({
+          target: collateralToken,
+          params: amm['id'],
+          abi: 'erc20:balanceOf',
+          block: block,
+        });
+
+      if (!polygon_result[collateralToken]) {
+        polygon_result[collateralToken] = '0';
+      }
+      polygon_result[collateralToken] = BigNumber(polygon_result[collateralToken]).plus(response.output).toFixed();
+  
+      // Get collateral in Series
+        const response2 = await sdk.api.abi.call({
+          target: collateralToken,
+          params: seriesVaults[0]['id'],
+          abi: 'erc20:balanceOf',
+          block: block,
+        });
+        polygon_result[collateralToken] = BigNumber(polygon_result[collateralToken]).plus(response2.output).toFixed();
+  
+    }
+    console.log(polygon_result);
+    return polygon_result;
+  
+  }
+  let tokenHolderMap = [];
+  async function tvl(timestamp, block) {
+    const amms = await request(POLYGON_GRAPH_URL, GET_POOLS_POLYGON, {
+      block,
+    })
+
+    const tokens = await getTokens( amms);
+    const holders = await getHolders( amms);
+    const balances = await calculatePolygonTVL(timestamp, block, amms);
+    tokenHolderMap = [
+      {
+        tokens,
+        holders,
+      },
+    ];
+    console.log(tokenHolderMap);
+    return balances;
+  }
+
+  const GET_POOLS_POLYGON = gql`
+  query Pools($block: Int) {
+    amms(block: { number: $block }) {
+      id
+      collateralToken {
+        id
+      }
+    }
+  }`
+
+  const GET_SERIES_VAULT_ID = gql`
+  query  {seriesVaults{
+          id
+          }
+  }`
+
+/*==================================================
+  Exports
+  ==================================================*/
+
+  module.exports = {
+    name: 'Siren',
+    token: 'SI',
+    category: 'Derivatives',
+    start: 1605574800, // Nov-17-2020 01:00:00 AM +UTC // tvl function should be kept above tokenHolderMap
+    tokenHolderMap: [{
+      tokens: [
+        '0xda537104d6a5edd53c6fbba9a898708e465260b6',
+        '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+        '0x85955046df4668e1dd369d2de9f3aeb98dd2a369',
+        '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+        '0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a',
+        '0x1c954e8fe737f99f68fa1ccda3e51ebdb291948c',
+        '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+        '0xb33eaad8d922b1083446dc23f610c2567fb5180f',
+        '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6'
+      ],
+      holders: [
+        '0x3deceb065c2d0aad57dca083e946f2e05a89580b',
+        '0x41667758d84c1a6c89e4422fce6453bb376ddb8d',
+        '0x5848311b010fb960f2aa1354833d3a887cb44fb6',
+        '0xa42e6396c4a66c764d5e86f1ba6b68c3b6427f64',
+        '0xa77c6917b2399cb99391f7ce32a4d9658de6ae4e',
+        '0xb16bfebfbebf7b5f91bf04782b1eaaeeb400b9d4',
+        '0xb54dfba46af632e865c3cec53f4e631307fa22ef',
+        '0xc3f7250f458e86a4bd19d0819550ac0b17902bdc',
+        '0xedd2a39b0a4770e61eb7998f371522169d253905',
+        '0xc40a31bd9fed1569ce647bb7de7ff93facca36e9'
+      ]
+    }],
+    chain: "polygon"
+  }
---
+++ v2/projects/siren/index.js
@@ -0,0 +1,140 @@
+/*==================================================
+  Modules
+  ==================================================*/
+  const { request, gql } = require("graphql-request");
+  const BigNumber = require('bignumber.js');
+  const axios = require('axios');
+  const sdk = require('../../../sdk');
+
+  const GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/sirenmarkets/protocol';
+
+
+/*==================================================
+  TVL
+  ==================================================*/
+
+  async function getTokens(amms) {
+
+    let tokens =[];
+    console.log(amms);
+    for (let i = 0; i < amms['amms'].length; i++) {
+      let amm = amms['amms'][i]
+      tokens.push(amm['collateralToken']['id']);
+
+    }
+    return tokens;
+  }
+
+  async function getHolders(amms) {
+
+    let holders =[];
+
+    for (let i = 0; i < amms['amms'].length; i++) {
+      let amm = amms['amms'][i]
+  
+      holders.push(amm['id']);
+      for (let im = 0; im < amm.markets.length; im++) {
+        const market = amm.markets[im];
+
+        holders.push(market['id'])
+
+       
+      }
+
+    }
+
+    return holders;
+  }
+
+  async function calculateMainnetTVL(timestamp, block, mainnet_amms) {
+  
+
+  
+    const mainnet_result = {
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '0'
+    };
+
+        for (let i = 0; i < mainnet_amms['amms'].length; i++) {
+
+        const amm = mainnet_amms['amms'][i];
+        const collateralToken = amm['collateralToken']['id'];
+        // Get collateral in AMM
+        let  response;
+        try {
+          response = await sdk.api.erc20.balanceOf({
+            block,
+            target: collateralToken,
+            owner: amm['id'],
+          });
+        }
+        catch(err) { 
+          console.log(err)
+        }
+        if (!mainnet_result[collateralToken]) {
+          mainnet_result[collateralToken] = '0';
+        }
+        mainnet_result[collateralToken] = BigNumber(mainnet_result[collateralToken]).plus(response.output).toFixed();
+  
+        // Get collateral in Markets
+        for (let im = 0; im < amm.markets.length; im++) {
+          const market = amm.markets[im];
+  
+          const response = await sdk.api.erc20.balanceOf({
+            block: block,
+            target: collateralToken,
+            owner: market['id']
+          });
+  
+          mainnet_result[collateralToken] = BigNumber(mainnet_result[collateralToken]).plus(response.output).toFixed();
+        }
+      }
+      return mainnet_result;
+  
+  }
+
+  const GET_POOLS = gql`
+    query Pools($block: Int) {
+      amms(block: { number: $block }) {
+        id
+        collateralToken {
+          id
+        }
+        markets {
+          id
+        }
+      }
+    }
+  `
+let tokenHolderMap = [];
+async function tvl(timestamp, block) {
+
+  const amms = await request(GRAPH_URL, GET_POOLS, {
+    block,
+  })
+  const tokens = await getTokens( amms);
+  const holders = await getHolders( amms);
+  const balances = await calculateMainnetTVL(timestamp, block, amms)
+  tokenHolderMap = [
+    {
+      tokens,
+      holders,
+    },
+  ];
+  console.log("MAINNENT", balances);
+  return balances;
+}
+
+/*==================================================
+  Exports
+  ==================================================*/
+
+  module.exports = {
+    name: 'Siren',
+    token: 'SI',
+    category: 'Derivatives',
+    start: 1605574800, // Nov-17-2020 01:00:00 AM +UTC
+    chain: 'ethereum',
+    tvl,
+    tokenHolderMap,
+
+  }
