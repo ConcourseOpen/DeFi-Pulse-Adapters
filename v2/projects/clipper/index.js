7ac7ec04be66816238ceb1dbb3b07133f9b6887d
l0c4t0r <l0c4t0r@gmail.com>
2022-01-07 23:15:23 +0800

    Fix merge conflict

--- v2/projects/clipper/index.js
+++ v2/projects/clipper/index.js
@@ -11,11 +11,7 @@ module.exports = {
   /* Project Metadata */
   name: "Clipper",
   token: null,
-<<<<<<< HEAD
-  category: "DEX/AMM",
-=======
   category: "DEXes",
->>>>>>> 76bf44061a96535ca451857abd2418e9131fb4b2
   start: 1624383620, // Unix timestamp (utc 0) specifying when the project began, or where live data begins
   chain: "ethereum",
 
